09b9ed22bd214dd47ee972f0196b11ea8a6c1c10
Marcio <dev@marciof.com>
2010-10-31 13:25:45 +0100

    - Added link to the Eon project page. - Replaced link to the SVN repository with the Git one.

--- Pages/Home.lisp
+++ Pages/Home.lisp
@@ -1,6 +1,6 @@
 (XML
   (:p "Welcome!")
   
-  (:p "If you have any questions, just "
-      (:a (@ (:class "Email")) "send me")
+  (:p "If you need to contact me "
+      (:a (@ (:class "Email")) "send")
       " an e-mail."))
--- Pages/Projects.lisp
+++ Pages/Projects.lisp
@@ -1,13 +1,13 @@
 (XML
-  (:p "To check out this web site's code and other minor random projects, visit "
-      (:a (@ (:href "http://www.assembla.com/code/marciof/subversion/nodes"))
-          "my public SVN repository")
+  (:p "To check out this web site's code and other random projects, visit "
+      (:a (@ (:href "http://github.com/marciof"))
+          "my Git repository")
       ".")
   
   (:h3 "Projects")
   
   (:dl
-    (:dt "Eon")
+    (:dt (:a (@ (:href "http://eon.origo.ethz.ch/")) "Eon"))
     (:dd "Kernel experiment to learn more about the design and implementation of Operating Systems.")
     
     (:dt (:a (@ (:href "http://janus.sourceforge.net/")) "Janus"))
