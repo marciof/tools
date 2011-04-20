f2b753fb64cdf273f61ca7adeb660499858d86c7
Marcio <dev@marciof.com>
2011-04-20 16:01:47 +0200

    Search the uninstall registry key as well.

--- Code/Browser/Application/Opera/Windows.pm
+++ Code/Browser/Application/Opera/Windows.pm
@@ -10,6 +10,8 @@ use Browser::Application::Opera ();
 extends 'Browser::System::Windows';
 
 const my $EXECUTABLE_FILE = 'opera.exe';
+const my $PATH_KEY = 'InstallLocation';
+const my $UNINSTALL_KEY = 'HKEY_LOCAL_MACHINE/SOFTWARE/Wow6432Node/Microsoft/Windows/CurrentVersion/Uninstall';
 const my $VENDOR_KEY = 'HKEY_CURRENT_USER/Software/Opera Software';
 
 
@@ -32,17 +34,13 @@ sub find_in_file_system {
 
 sub find_in_registry {
     my ($self) = @ARG;
-    my $information = $self->registry->{$VENDOR_KEY};
     my @browsers;
+    my @paths = (
+        $self->_find_in_registry_install,
+        $self->_find_in_registry_uninstall,
+    );
     
-    $self->logger->debug("Registry search: $VENDOR_KEY");
-    
-    while (my ($key, $value) = each %$information) {
-        next unless $key =~ m/\b (directory | path) \b/ix;
-        
-        my $path = Path::Class::File->new($value, $EXECUTABLE_FILE);
-        next unless -e $path;
-        
+    foreach my $path (@paths) {
         push @browsers, Browser::Application::Opera->new(
             path => $path,
             system => $self,
@@ -50,4 +48,38 @@ sub find_in_registry {
     }
     
     return @browsers;
+}
+
+
+sub _find_in_registry_install {
+    my ($self) = @ARG;
+    my $information = $self->registry->{$VENDOR_KEY};
+    my @paths;
+    
+    $self->logger->debug("Registry search: $VENDOR_KEY");
+    
+    while (my ($key, $value) = each %$information) {
+        if ($key =~ m/\b (directory | path) \b/ix) {
+            my $path = Path::Class::File->new($value, $EXECUTABLE_FILE);
+            push @paths, $path if -e $path;
+        }
+    }
+    
+    return @paths;
+}
+
+
+sub _find_in_registry_uninstall {
+    my ($self) = @ARG;
+    my $uninstall = $self->registry->{$UNINSTALL_KEY};
+    my @paths;
+    
+    $self->logger->debug("Registry search: $UNINSTALL_KEY");
+    
+    foreach my $application (grep m/^Opera\b/, $uninstall->SubKeyNames) {
+        my $path = $uninstall->{$application}{$PATH_KEY};
+        push @paths, Path::Class::File->new($path, $EXECUTABLE_FILE);
+    }
+    
+    return @paths;
 }
