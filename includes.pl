399e9a15b58703f12506d94ada24c317a9fc9dec
Marcio <dev@marciof.com>
2011-09-16 13:23:34 +0200

    Add script to detect simple duplicate and unused Perl module inclusions.

---
+++ includes.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/env perl
+
+use defaults;
+use File::Slurp ();
+use List::MoreUtils ();
+use PPI ();
+
+
+my $doc = PPI::Document->new(@ARGV == 0
+    ? File::Slurp::slurp(\*STDIN)
+    : $ARGV[0]);
+
+my $includes = $doc->find(
+    sub {
+        my ($root, $element) = @ARG;
+        return $element->isa('PPI::Statement::Include')
+            && ($element->type ne 'no')
+            && ($element->pragma eq '')
+            && ($element->version eq '')
+    
+    });
+
+my $words = $doc->find(
+    sub {
+        my ($root, $element) = @ARG;
+        return $element->isa('PPI::Token::Word')
+            && !$element->parent->isa('PPI::Statement::Include');
+    });
+
+my %uniq_includes;
+
+foreach my $include (@$includes) {
+    my $module = $include->module;
+    
+    if (exists $uniq_includes{$module}) {
+        my ($line, $column) = ($include->line_number, $include->column_number);
+        
+        say "Duplicate $module at line $line"
+            . ($column > 1 ? ", column $column." : '.');
+    }
+    else {
+        $uniq_includes{$module} = $include;
+    }
+}
+
+foreach my $include (@$includes) {
+    my $module = $include->module;
+    next if List::MoreUtils::any {$ARG =~ m/^ \Q$module\E \b/x} @$words;
+    
+    my ($line, $column) = ($include->line_number, $include->column_number);
+    say "Unused $module at line $line"
+            . ($column > 1 ? ", column $column." : '.');
+}
