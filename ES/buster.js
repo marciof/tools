c2700e07ef5975a2544b2d719d56c8a82555805d
Marcio <dev@marciof.com>
2012-07-28 17:51:04 +0200

    Initial conversion to Buster tests.

---
+++ ES/buster.js
@@ -0,0 +1,18 @@
+var config = module.exports;
+
+config['Node tests'] = {
+    environment: 'node',
+    sources: ['*.js'],
+    tests: ['*.test.js']
+};
+
+
+require('buster').assertions.add('isUndefined', {
+    assertMessage: 'Expected ${0} to be undefined',
+    refuteMessage: 'Expected ${0} to not be undefined',
+    
+    assert: function (value) {
+        // Don't use `undefined` in case it has been redefined.
+        return value === void(0);
+    }
+});
