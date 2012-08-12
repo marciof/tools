05651cb670640ff4a5300e790d0aca6c8d69e3a6
Marcio <dev@marciof.com>
2012-08-12 22:52:00 +0200

    Remove alternate INF code.

--- mouse_shadow.cpp
+++ mouse_shadow.cpp
@@ -10,21 +10,3 @@ int main() {
     
     return didSucceed ? EXIT_SUCCESS : EXIT_FAILURE;
 }
-
-
-/*
---- Via INF file ---
-
-[Version]
-Signature=$CHICAGO$
-
-[DefaultInstall]
-AddReg=Reg.Settings
-BitReg=Bit.Settings
-
-[Reg.Settings]
-HKLM,Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects\CursorShadow,DefaultValue,0x00010001,1
-
-[Bit.Settings]
-HKCU,"Control Panel\Desktop",UserPreferencesMask,1,0x20,1
-*/
