7bb60e7dd0ab01aa900d3ef2179d6a8a95c74110
Marcio <dev@marciof.com>
2012-08-13 20:14:04 +0200

    Add an "assert.throws" like "assert.exception" but which uses "instanceof".

--- ES/buster.js
+++ ES/buster.js
@@ -1,4 +1,8 @@
-var config = module.exports;
+'use strict';
+
+
+var config = module.exports,
+    assertions = require('buster').assertions;
 
 config['Node based tests'] = {
     environment: 'node',
@@ -7,12 +11,32 @@ config['Node based tests'] = {
 };
 
 
-require('buster').assertions.add('isUndefined', {
+assertions.add('isUndefined', {
     assertMessage: 'Expected ${0} to be undefined',
     refuteMessage: 'Expected ${0} to not be undefined',
     
     assert: function (value) {
         // Don't use `undefined` in case it has been redefined.
         return value === void(0);
+    }
+});
+
+
+assertions.add('throws', {
+    assertMessage: 'Expected ${name} to be thrown (got ${actualName})',
+    refuteMessage: 'Expected ${name} to not be thrown',
+    
+    assert: function (fn, Exception) {
+        this.name = '`' + Exception.prototype.name + '`';
+        
+        try {
+            fn();
+            this.actualName = 'nothing';
+            return false;
+        }
+        catch (exception) {
+            this.actualName = '`' + exception.name + '`';
+            return exception instanceof Exception;
+        }
     }
 });
