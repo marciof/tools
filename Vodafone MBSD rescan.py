2735e954de440ecff9abfdfbe8eb14abd00a6a5e
Marcio <dev@marciof.com>
2012-12-24 18:34:35 +0000

    Add simple GUI.

--- Vodafone MBSD rescan.py
+++ Vodafone MBSD rescan.py
@@ -3,21 +3,31 @@
 
 
 # Standard:
+import Tkinter
+import tkMessageBox
 from urllib import urlencode
 from urllib2 import urlopen
 
 
+TITLE = 'Vodafone MBSD'
 ADMIN_URL = 'http://VodafoneMobileBroadband.SharingDock'
 ADMIN_PASSWORD = 'admin'
 
 
-try:
-    urlopen(ADMIN_URL + '/cgi-bin/login.cgi', data = urlencode({
-        'AdPassword': ADMIN_PASSWORD,
-    }))
-    
-    urlopen(ADMIN_URL + '/cgi-bin/usb_modem.cgi', data = urlencode({
-        'rescan': '1',
-    }))
-finally:
-    urlopen(ADMIN_URL + '/cgi-bin/logout.cgi', data = urlencode({}))
+Tkinter.Tk().wm_withdraw()
+
+if tkMessageBox.askyesno(TITLE, 'Start rescan?'):
+    try:
+        urlopen(ADMIN_URL + '/cgi-bin/login.cgi', data = urlencode({
+            'AdPassword': ADMIN_PASSWORD,
+        }))
+        
+        urlopen(ADMIN_URL + '/cgi-bin/usb_modem.cgi', data = urlencode({
+            'rescan': '1',
+        }))
+    except BaseException:
+        tkMessageBox.showerror(TITLE, 'Rescan error.')
+    else:
+        tkMessageBox.showinfo(TITLE, 'Rescan started.')
+    finally:
+        urlopen(ADMIN_URL + '/cgi-bin/logout.cgi', data = urlencode({}))
