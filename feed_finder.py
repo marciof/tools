041e92a2f3139839472f6b67db663ab8642a8c8c
Marcio <dev@marciof.com>
2013-10-16 14:48:24 +0200

    Find default sitemap and walk up URL's.

--- feed_finder.py
+++ feed_finder.py
@@ -18,6 +18,7 @@
 # TODO: Leverage `feedfinder2`?
 # TODO: Detect Atom feeds.
 # TODO: Convert all input to Unicode.
+# TODO: Verify URL's point to actual Sitemaps? http://www.esquire.com.tr
 # TODO: Verify URL's point to actual feeds? E.g. http://sphinx-doc.org/rss
 # TODO: Scrape HTML index pages? E.g. http://www.journaldequebec.com/rss
 # TODO: Check <LINK> elements?
@@ -49,22 +50,42 @@ class Feed (object):
         self.title = title
 
 
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+    
+    
+    def __hash__(self):
+        return hash(self.url)
+
+
     def __unicode__(self):
-        return '%s <%s> %s %s' % (
-            '?' if self.title is None else repr(self.title),
-            self.url,
-            self.mime_type,
-            '?' if self.relation is None else repr(self.relation))
+        if self.title is None:
+            return self.url
+        else:
+            return '%s <%s>' % (self.title, self.url)
 
 
 class Finder (object):
     def __init__(self):
         self._logger = logging.getLogger(__name__)
         
+        self._ignore_xml_ns = True
         self._rss_rel = 'alternate'
-        self._strict_link_el_location = False
+        self._strict_link_el_loc = False
         self._strict_mime_types = False
         self._strict_rss_rel = False
+        self._walk_urls_up = True
+
+
+    def find(self, url):
+        if self._walk_urls_up:
+            parent_urls = self._walk_url_up(url)
+        else:
+            parent_urls = [url]
+        
+        for parent_url in parent_urls:
+            for feed in self.find_in_html(parent_url):
+                yield feed
 
 
     def find_in_anchor_links(self, html, base_url):
@@ -73,7 +94,7 @@ class Finder (object):
         for anchor in html.xpath('//a[@href]'):
             url = anchor.get('href')
 
-            if self.is_feed_like_url(url):
+            if self._is_feed_like_url(url):
                 title = anchor.get('title')
 
                 if title is None:
@@ -88,7 +109,7 @@ class Finder (object):
         self._logger.debug('Finding in auto-discovery')
         link_xpath = 'link[@type]'
         
-        if self._strict_link_el_location:
+        if self._strict_link_el_loc:
             link_xpath = '/html/head/' + link_xpath
         else:
             link_xpath = '//' + link_xpath
@@ -132,59 +153,64 @@ class Finder (object):
     def find_in_html(self, url):
         html = lxml.html.parse(url).getroot()
         base_url = html.base_url
-
+        
         if base_url is None:
             base_url = url
-        
+
         return itertools.chain(
             self.find_in_auto_discovery(html, base_url),
             self.find_in_anchor_links(html, base_url))
 
 
-    def find_sitemaps(self, url):
-        return set(
-            self.find_sitemaps_in_robots_txt(url)
-            + self.find_sitemaps_in_indices(url))
-
-
-    def find_sitemaps_in_indices(self, url):
-        self._logger.debug('Finding sitemaps from sitemap index')
-
-        url = purl.URL(url).add_path_segment('sitemap_index.xml')
-        urls = []
-
-        # Ignore XML namespaces.
-        SITEMAP = '*[local-name() = "sitemap"]'
-        LOC = '*[local-name() = "loc"]'
-
-        while True:
-            self._logger.debug('Checking for sitemap index: %s', url)
-
-            try:
-                index = lxml.etree.parse(unicode(url))
-                urls.extend(index.xpath('//%s/%s/text()' % (SITEMAP, LOC)))
-            except IOError:
-                pass
-
-            segments = list(url.path_segments())
-
-            if len(segments) > 1:
-                segments.pop(-2)
-                url = url.path_segments(segments)
-            else:
-                break
-
+    def _find_sitemaps(self, url):
+        parsed_url = purl.URL(url)
+        urls = set(self._find_sitemaps_in_robots_txt(url))
+        
+        urls.update(self._find_sitemaps_in_index(parsed_url))
+        urls.add(unicode(parsed_url.add_path_segment('sitemap.xml')))
+        
         return urls
 
 
-    def find_sitemaps_in_robots_txt(self, url):
-        self._logger.debug('Finding sitemaps from robots.txt')
+    def _find_sitemaps_in_index(self, url):
+        self._logger.debug('Finding sitemaps in sitemap index')
+        
+        url = unicode(url.add_path_segment('sitemap_index.xml'))
+        (sitemap_tag, loc_tag) = ('sitemap', 'loc')
+        
+        if self._ignore_xml_ns:
+            sitemap_tag = '*[local-name() = "%s"]' % sitemap_tag
+            loc_tag = '*[local-name() = "%s"]' % loc_tag
+
+        try:
+            index = lxml.etree.parse(url)
+        except IOError:
+            return []
+        else:
+            return index.xpath('//%s/%s/text()' % (sitemap_tag, loc_tag))
+
+
+    def _find_sitemaps_in_robots_txt(self, url):
+        self._logger.debug('Finding sitemaps in robots.txt')
         return reppy.cache.RobotsCache().fetch(url).sitemaps
 
 
-    def is_feed_like_url(self, url):
+    def _is_feed_like_url(self, url):
         segments = set(purl.URL(url.lower()).path_segments())
         return ('feed' in segments) or ('rss' in segments)
+    
+    
+    def _walk_url_up(self, url):
+        url = purl.URL(url)
+        segments = list(url.path_segments())
+        
+        while True:
+            yield unicode(url.path_segments(segments))
+            
+            if len(segments) > 0:
+                segments.pop()
+            else:
+                break
 
 
 if __name__ == '__main__':
@@ -197,6 +223,14 @@ if __name__ == '__main__':
     logging.basicConfig(level = logging.DEBUG)
     (url,) = args
     finder = Finder()
+    seen_feeds = set()
 
-    for feed in finder.find_in_html(url):
-        print unicode(feed)
+    for sitemap_url in finder._find_sitemaps(url):
+        print ('\t' + sitemap_url).encode('UTF-8')
+
+    print 30 * '-'
+    
+    for feed in finder.find(url):
+        if feed not in seen_feeds:
+            print ('\t%s' % feed).encode('UTF-8')
+            seen_feeds.add(feed)
