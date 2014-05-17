d37c82d8c3856e6a05a0ac29cc11efbb55243022
Marcio <dev@marciof.com>
2014-05-17 14:06:53 +0200

    Remove infrequently used IPython config.

--- ipython_config.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-# Standard:
-from __future__ import absolute_import, division, unicode_literals
-
-c = get_config()
-c.InteractiveShell.colors = 'LightBG'
-c.TerminalInteractiveShell.confirm_exit = False
-c.TerminalIPythonApp.display_banner = False
