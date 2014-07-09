867e6f4ba65577abcf735927339846f9c531ec30
Marcio <dev@marciof.com>
2014-07-09 09:10:34 +0200

    Revert "Remove infrequently used IPython config."
    This reverts commit d950075c7f62eb2e19e0ab385d00a8625d277e04.

---
+++ ipython_config.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*-
+
+# Standard:
+from __future__ import absolute_import, division, unicode_literals
+
+c = get_config()
+c.InteractiveShell.colors = 'LightBG'
+c.TerminalInteractiveShell.confirm_exit = False
+c.TerminalIPythonApp.display_banner = False
