f9b52fe9991f499ebb78b71e2b6ec0c047eb5179
Marcio <dev@marciof.com>
2016-02-20 20:21:17 +0100

    Add Linked List data type.

---
+++ src/list/Linked_List.h
@@ -0,0 +1,16 @@
+#ifndef SHOW__LIST__LINKED_LIST_H
+#define SHOW__LIST__LINKED_LIST_H
+
+
+#include "List.h"
+
+
+/**
+ * Doubly linked linear list implementation.
+ *
+ * The sorting algorithm is stable and is O(n log n) in time and O(1) in memory.
+ */
+extern const List_Impl Linked_List;
+
+
+#endif
