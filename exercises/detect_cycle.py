6618709ca637b3060c7a0f74b0d1e88c5cc4c551
Marcio <dev@marciof.com>
2016-03-24 15:48:02 +0100

    Return node that can stop the cycle.

--- exercises/detect_cycle.py
+++ exercises/detect_cycle.py
@@ -1,6 +1,42 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+"""
+Floyd's "tortoise and hare" algorithm:
+
+1. Keep two pointers: `slow` steps one node at a time, `fast` steps double.
+2. If they ever meet, then there's a cycle.
+3. Otherwise, there's no cycle.
+
+To find the start of the cycle:
+
+1. Reset `slow` to the start.
+2. Step both `slow` and `fast` now one node at a time.
+3. The node where they meet is the start of the cycle.
+
+Why does this work?
+
+Imagine two people racing around a track, one twice as fast as the other. If
+they start off at the same place, they will next meet at the start of the next
+lap.
+
+After some moves both pointers will be in the cycle. With the next moves you
+can be sure that both will meet at some point since with every step the
+difference between them reduces by 1.
+
+Hypothesis:
+
+- `m` = distance from first node to start of cycle (# nodes)
+- `l` = length of cycle (# nodes)
+- `k` = distance of meeting point from start of cycle (# nodes)
+- `dist_S = m + p*l + k`
+- `dist_F = m + q*l + k` where `q > p` since this one goes faster
+- `2 * dist_S = dist_F` (note that any multiple would work)
+- `2 * (m + p*l + k) = m + q*l + k`
+- `m + 2*p*l + k = q*l`
+- `m + k = (q - 2*p)*l` implies `m+k` is an integer multiple of the cycle length
+"""
+
 import unittest
 
 class Node:
@@ -43,6 +79,8 @@ def print_list(l):
 def has_cycle(l):
     """
     Detect a cycle in a linked list. (Floyd's "tortoise and hare" algorithm.)
+
+    Returns `(has_cycle, cycle start node, previous node)`.
     """
 
     def step(node):
@@ -50,13 +88,15 @@ def has_cycle(l):
 
     slow = l
     fast = l
+    previous = None
 
     while True:
         slow = step(slow)
-        fast = step(step(fast))
+        previous = step(fast)
+        fast = step(previous)
 
         if fast is None:
-            return (False, None)
+            return (False, None, None)
 
         if slow == fast:
             break
@@ -65,40 +105,49 @@ def has_cycle(l):
 
     while slow != fast:
         slow = step(slow)
+        previous = fast
         fast = step(fast)
 
-    return (True, slow)
+    return (True, slow, previous)
 
 class Test (unittest.TestCase):
     def test_empty_list(self):
-        self.assertEqual(has_cycle(make_list()), (False, None))
+        self.assertEqual(
+            has_cycle(make_list()),
+            (False, None, None))
 
     def test_single_node(self):
-        self.assertEqual(has_cycle(make_list('a')), (False, None))
+        self.assertEqual(
+            has_cycle(make_list('a')),
+            (False, None, None))
 
     def test_multiple_nodes(self):
-        self.assertEqual(has_cycle(make_list('a', 'b', 'c')), (False, None))
+        self.assertEqual(
+            has_cycle(make_list('a', 'b', 'c')),
+            (False, None, None))
 
     def test_multiple_repeated_nodes(self):
-        self.assertEqual(has_cycle(make_list('a', 'a', 'a')), (False, None))
+        self.assertEqual(
+            has_cycle(make_list('a', 'a', 'a')),
+            (False, None, None))
 
     def test_cycle_with_single_node(self):
         l = make_list('x')
         l.next = l
-        self.assertEqual(has_cycle(l), (True, l))
+        self.assertEqual(has_cycle(l), (True, l, l))
 
     def test_cycle_with_multiple_nodes(self):
         l = make_list('a', 'b', 'c')
         c = l.next.next
 
         c.next = l
-        self.assertEqual(has_cycle(l), (True, l))
+        self.assertEqual(has_cycle(l), (True, l, c))
 
         c.next = l.next
-        self.assertEqual(has_cycle(l), (True, l.next))
+        self.assertEqual(has_cycle(l), (True, l.next, c))
 
         c.next = l.next.next
-        self.assertEqual(has_cycle(l), (True, l.next.next))
+        self.assertEqual(has_cycle(l), (True, l.next.next, c))
 
 if __name__ == '__main__':
     unittest.main(verbosity = 2)
