a3c350ce985905cdb92056a2acdab2aa884dc6df
Marcio <dev@marciof.com>
2016-04-07 22:33:49 +0200

    HackerRank version.

--- puzzles/nth_last_node.py
+++ puzzles/nth_last_node.py
@@ -1,55 +1,68 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-import collections
+import io
+import re
+import sys
 import unittest
 
-Node = collections.namedtuple('Node', ['value', 'next'])
+VALUE = 0
+NEXT = 1
+
+def nth_last_node(lst, n):
+    last_node = lst
+    curr_node = lst
+    pos = 0
+
+    while pos < n:
+        if curr_node is None:
+            return None
+
+        pos += 1
+        curr_node = curr_node[NEXT]
+
+    while curr_node is not None:
+        last_node = last_node[NEXT]
+        curr_node = curr_node[NEXT]
+
+    return last_node
+
+def solve_from_input(file_in):
+    m = int(file_in.readline())
+    head = [None, None]
+    last_node = head
+    last_value = ''
+
+    while True:
+        buffer = file_in.read(io.DEFAULT_BUFFER_SIZE)
+
+        if not buffer:
+            break
+
+        values = re.split(r'\b \b', last_value + buffer)
+        last_value = values.pop()
+
+        for value in values:
+            node = [int(value), None]
+            last_node[NEXT] = node
+            last_node = node
+
+    if last_value:
+        node = [int(last_value), None]
+        last_node[NEXT] = node
+
+    nth_node = nth_last_node(head[NEXT], m)
+
+    if nth_node is None:
+        print('NIL')
+    else:
+        print(nth_node[VALUE])
 
 def make_list(*values):
     if len(values) == 0:
         return None
-
-    return Node(
-        value = values[0],
-        next = make_list(*values[1:]))
-
-def print_list(l):
-    while l is not None:
-        print(l.value, end ='')
-
-        if l.next:
-            print(', ', end = '')
-            l = l.next
-        else:
-            break
-
-    print()
-
-def nth_last_node(l, n):
-    """
-    Return the `n`-th last node in a singly-linked list `l`.
-
-    Time: O(n)
-    Space: O(1)
-    """
-
-    last = l
-    current = l
-    pos = 0
-
-    while pos < n:
-        if current is None:
-            return None
-
-        pos += 1
-        current = current.next
-
-    while current is not None:
-        last = last.next
-        current = current.next
-
-    return last
+    else:
+        return [values[0], make_list(*values[1:])]
 
 class Test (unittest.TestCase):
     def test_empty_list(self):
@@ -58,14 +71,17 @@ class Test (unittest.TestCase):
     def test_single_element(self):
         l = make_list('x')
         self.assertIsNone(nth_last_node(l, 2))
-        self.assertEqual(nth_last_node(l, 1).value, 'x')
+        self.assertEqual(nth_last_node(l, 1)[VALUE], 'x')
 
     def test_multiple_elements(self):
         l = make_list('x', 'y', 'z')
         self.assertIsNone(nth_last_node(l, 4))
-        self.assertEqual(nth_last_node(l, 3).value, 'x')
-        self.assertEqual(nth_last_node(l, 2).value, 'y')
-        self.assertEqual(nth_last_node(l, 1).value, 'z')
+        self.assertEqual(nth_last_node(l, 3)[VALUE], 'x')
+        self.assertEqual(nth_last_node(l, 2)[VALUE], 'y')
+        self.assertEqual(nth_last_node(l, 1)[VALUE], 'z')
 
 if __name__ == '__main__':
-    unittest.main(verbosity = 2)
+    if sys.stdin.isatty():
+        unittest.main(verbosity = 2)
+    else:
+        solve_from_input(sys.stdin)
