4d37512619674151c2feaf08499648ebbfcacfee
Marcio <dev@marciof.com>
2016-04-10 20:02:28 +0200

    Start X server and the needed docker-machine automatically

--- docker/clion.cmd
+++ docker/clion.cmd
@@ -1,3 +1,5 @@
+START x11.xlaunch
+docker-machine start default
 FOR /F "tokens=*" %%i IN ('docker-machine env default --shell cmd') DO %%i
 FOR /F %%i IN ('hostname') DO SET DISPLAY=%%i:0
 docker-compose -f clion\docker-compose.yml up -d
