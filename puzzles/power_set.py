6fe424485fc20106f27d95f90153459aca01cbaf
Marcio <dev@marciof.com>
2016-05-01 22:00:08 +0200

    Puzzle on listing the power set of a set of numbers.

---
+++ puzzles/power_set.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import unittest
+
+def generate(numbers):
+    """
+    List all subsets of a given set of `numbers`.
+
+    Time: O(n)
+    """
+
+    if len(numbers) == 0:
+        return [set()]
+
+    combinations = []
+
+    for combination in generate(numbers[1:]):
+        combinations.append(combination)
+        combinations.append({numbers[0]} | combination)
+
+    return combinations
+
+class Test(unittest.TestCase):
+    def test_many(self):
+        self.assertCountEqual(
+            generate([1, 2, 3]),
+            [set(), {1}, {2}, {3}, {1, 2}, {2, 3}, {1, 3}, {1, 2, 3}])
+
+    def test_empty(self):
+        self.assertCountEqual(
+            generate([]),
+            [set()])
+
+    def test_single(self):
+        self.assertCountEqual(
+            generate([1]),
+            [set(), {1}])
+
+if __name__ == '__main__':
+    unittest.main(verbosity = 2)
