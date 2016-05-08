3d968217653c336c7655fb7af72c1ab4690b61e9
Marcio <dev@marciof.com>
2016-05-08 22:00:32 +0200

    Puzzle on generating all combinations from a pattern string of 0 and 1

---
+++ puzzles/wildcard_permutate.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import unittest
+
+def generate(pattern):
+    """
+    Generate all combinations of the `pattern` string, consisting of `'0'` and
+    `'1'`, where `'?'` is a wildcard.
+    """
+
+    if pattern == '':
+        return ['']
+
+    combinations = []
+    prefixes = pattern[:1]
+    suffix = pattern[1:]
+
+    if prefixes == '?':
+        prefixes = {'0', '1'}
+
+    for combination in generate(suffix):
+        for prefix in prefixes:
+            combinations.append(prefix + combination)
+
+    return combinations
+
+class Test(unittest.TestCase):
+    def test_fixed_string(self):
+        self.assertCountEqual(
+            generate('01101'),
+            {'01101'})
+
+    def test_single_wildcard(self):
+        self.assertCountEqual(
+            generate('011?0'),
+            {'01100', '01110'})
+
+    def test_multiple_wildcard(self):
+        self.assertCountEqual(
+            generate('011?0?'),
+            {'011000', '011100', '011001', '011101'})
+
+    def test_empty_string(self):
+        self.assertCountEqual(
+            generate(''),
+            {''})
+
+    def test_wildcard_string(self):
+        self.assertCountEqual(
+            generate('??'),
+            {'00', '01', '10', '11'})
+
+if __name__ == '__main__':
+    unittest.main(verbosity = 2)
