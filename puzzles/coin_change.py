96461ec33dbad9c5a9bca44ec4a576232d4519f7
Marcio <dev@marciof.com>
2016-05-29 21:40:36 +0200

    Puzzle on generating all possible coin combinations for a given amount

---
+++ puzzles/coin_change.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+from collections import Counter
+import unittest
+
+def generate(amount, coins):
+    """
+    Generate all possible coin combinations using any from `coins`
+    that when added equal `amount`.
+
+    https://programmingpraxis.com/2013/05/17/coin-change-part-1/2/
+    """
+
+    combs = [[] for _ in range(amount + 1)]
+    combs[0] = [[]]
+
+    for coin in coins:
+        i = coin
+
+        while i <= amount:
+            for comb in combs[i - coin]:
+                combs[i].append([coin] + comb)
+
+            i += 1
+
+    return combs[amount]
+
+class Test(unittest.TestCase):
+    def test_few_combinations(self):
+        self.assertCountEqual(
+
+            map(Counter,
+                generate(10, [2, 3])),
+
+            map(Counter, [
+                [2, 2, 2, 2, 2],
+                [2, 2, 3, 3],
+            ]))
+
+    def test_some_combinations(self):
+        self.assertCountEqual(
+
+            map(Counter,
+                generate(4, [3, 1, 2])),
+
+            map(Counter, [
+                [1, 1, 1, 1],
+                [1, 1, 2],
+                [2, 2],
+                [1, 3],
+            ]))
+
+    def test_many_combinations(self):
+        self.assertCountEqual(
+
+            map(Counter,
+                generate(10, [2, 5, 3, 6])),
+
+            map(Counter, [
+                [2, 2, 2, 2, 2],
+                [2, 2, 3, 3],
+                [2, 2, 6],
+                [2, 3, 5],
+                [5, 5],
+            ]))
+
+if __name__ == '__main__':
+    unittest.main(verbosity = 2)
