7acc3e94f0c2d9a1887338aee800c0ef8a0c50a0
Marcio <dev@marciof.com>
2016-06-27 10:05:55 +0200

    Remove unused Windows-specific Docker files

--- docker/idea.cmd
+++
@@ -1,5 +0,0 @@
-START x11.xlaunch
-docker-machine start default
-FOR /F "tokens=*" %%i IN ('docker-machine env default --shell cmd') DO %%i
-FOR /F %%i IN ('hostname') DO SET DISPLAY=%%i:0
-docker-compose -f idea\docker-compose.yml up -d
