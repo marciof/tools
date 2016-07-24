b189b13e80de02edaf8c7a008e6c54a346b3b415
Marcio <dev@marciof.com>
2016-07-24 22:22:49 +0200

    Cleanup for readability

--- puzzles/uncoupled_integer.py
+++ puzzles/uncoupled_integer.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 """
-Find the only unpaired integer in a sequence of `numbers`.
+Find the only unpaired integer in a sequence of `integers`.
 
 Time: O(n)
 Space: O(1)
@@ -11,10 +11,10 @@ Space: O(1)
 import sys
 import unittest
 
-def find_alone_int(numbers):
+def find(integers):
     target = 0
 
-    for n in numbers:
+    for n in integers:
         target ^= n
 
     return target
@@ -22,21 +22,21 @@ def find_alone_int(numbers):
 class Test (unittest.TestCase):
     def test_single_int(self):
         self.assertEqual(
-            find_alone_int((v for v in [9])),
+            find([9]),
             9)
 
     def test_multiple_ints(self):
         self.assertEqual(
-            find_alone_int((v for v in [1, 2, 3, 4, 5, 99, 1, 2, 3, 4, 5])),
+            find([1, 2, 3, 4, 5, 99, 1, 2, 3, 4, 5]),
             99)
 
     def test_negative_ints(self):
         self.assertEqual(
-            find_alone_int((v for v in [1, -123, -2, -2, 70, 1, 70])),
+            find([1, -123, -2, -2, 70, 1, 70]),
             -123)
 
 if __name__ == '__main__':
     if sys.stdin.isatty():
         unittest.main(verbosity = 2)
     else:
-        print(find_alone_int(map(int, sys.stdin.readline().split(', '))))
+        print(find(map(int, sys.stdin.readline().split(', '))))
