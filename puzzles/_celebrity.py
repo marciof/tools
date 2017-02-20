2d68f69a406027b2701dd2c4c04d16773cba9884
Marcio <dev@marciof.com>
2017-02-19 21:11:47 -0800

    Add puzzles to do

---
+++ puzzles/_celebrity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""
+In a party of one or more people, only one person is known to
+everyone. Such a person may be present in the party, if yes, (s)he
+doesn't know anyone in the party. We can only ask questions like "does
+A know B? “. Find the stranger (celebrity) in minimum number of
+questions.
+"""
---
+++ puzzles/_find_dups.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""
+Find all duplicates in an array where the numbers in the array are in the
+range of 0 to n-1 where n is the size of the array. For example: [1, 2, 3, 3]
+is okay but [1, 2, 6, 3] is not. In this version of the challenge there can
+be multiple duplicate numbers as well.
+"""
