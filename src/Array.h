9e825c9a97789c91460a6a0b20861b7f6439fa0e
Marcio <dev@marciof.com>
2017-03-10 19:32:17 -0800

    Initialize all Array struct members to avoid warnings

--- src/Array.h
+++ src/Array.h
@@ -7,7 +7,7 @@
     (sizeof(array) / sizeof((array)[0]))
 
 #define ARRAY_INITIAL_CAPACITY ((size_t) 8)
-#define ARRAY_NULL_INITIALIZER {NULL}
+#define ARRAY_NULL_INITIALIZER {NULL, {0}, 0, 0}
 
 #define /* bool */ ARRAY_IS_NULL_INITIALIZED(/* Array* */ array) \
     ((array)->data == NULL)
