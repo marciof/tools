8dff06fa257b805cdeff8a07b75b60dcf54c8c32
Marcio <dev@marciof.com>
2017-03-12 21:33:59 -0700

    Applying xsessionrc may sometimes fail, just ignore it

--- dbus-xsessionrc.sh
+++ dbus-xsessionrc.sh
@@ -2,14 +2,14 @@
 set -e -u
 
 app="$(basename "$0" .sh)"
-path=/org/freedesktop/login1
-interface=org.freedesktop.login1.Manager
+path='path=/org/freedesktop/login1'
+interface='interface=org.freedesktop.login1.Manager'
 
-dbus-monitor --system --profile "type=signal,path=$path,interface=$interface" 2>/dev/null |
-    while read type timestamp serial sender destination path interface member; do
+dbus-monitor --system --profile "type=signal,$path,$interface" 2>/dev/null |
+    while read type timestamp serial sender dest path interface member; do
         case "$member" in
             Session*)
-                logger --stderr --tag "$app" "Got $member: Re-apply"
-                sh ~/.xsessionrc
+                logger --stderr --tag "$app" "got $member: re-apply"
+                sh ~/.xsessionrc || true
         esac
     done
