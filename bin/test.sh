e92c54e8e35acc7bf4a40966fd339a66edf8a929
Marcio <dev@marciof.com>
2017-03-19 08:28:45 -0700

    Capture stderr when testing

--- bin/test.sh
+++ bin/test.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-set -u
+set -e -u
 
 for test_file; do
-    test_dir="$(dirname "$test_file")"
+    test_dir="$(readlink -e "$(dirname "$test_file")")"
     test_script="$test_dir/$(basename "$test_file" .t).sh"
+    test_output="$test_dir/$(basename "$test_file" .t).out"
 
     cat <<EOT > "$test_script"
 #!/bin/sh
 set -u
-TESTDIR="$(readlink -e "$test_dir")"
+TESTDIR="$test_dir"
 EOT
 
     chmod +x "$test_script"
@@ -16,12 +17,9 @@ EOT
     sed -E '/^  /! s/^/#/' < "$test_file" \
         | sed -E 's/^  \$ //' \
         | grep -v -E '^  ' \
-        | sed -E '/^#/! s/^(.+)$/cat <<"EOT"\n  $ \1\nEOT\ncat <<EOT | sh | sed -E "s:^:  :"\n\1\nEOT/' \
-        | sed -E "s/^#([^\\\$\"'-]*)$/echo \1/" \
+        | sed -E '/^#/! s/^(.+)$/cat <<"EOT"\n  $ \1\nEOT\ncat <<EOT | sh 2>\&1 | sed -E "s:^:  :"\n\1\nEOT/' \
+        | sed -E "s/^#([^\\\$\"\`'();{}-]*)$/echo \1/" \
         | sed -E 's/^#(.*)$/cat <<"EOT"\n\1\nEOT/' >> "$test_script"
 
-    echo "$test_script"
-    echo ==========
-    cat "$test_script"
-    echo ----------
+    sh "$test_script" > "$test_output"
 done
