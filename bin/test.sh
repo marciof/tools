c3dd39ccd217a0c91ade0a8b76d9dfec5dc76a34
Marcio <dev@marciof.com>
2017-03-19 09:38:22 -0700

    Handle other shell meta-characters

--- bin/test.sh
+++ bin/test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e -u
 
+# FIXME: Decouple into stages (compile .t to .sh, run .sh, diff output).
 for test_file; do
     test_dir="$(readlink -e "$(dirname "$test_file")")"
     test_script="$test_dir/$(basename "$test_file" .t).sh"
@@ -14,11 +15,12 @@ EOT
 
     chmod +x "$test_script"
 
+    # FIXME: Escape "EOT" when needed.
     sed -E '/^  /! s/^/#/' < "$test_file" \
         | sed -E 's/^  \$ //' \
         | grep -v -E '^  ' \
         | sed -E '/^#/! s/^(.+)$/cat <<"EOT"\n  $ \1\nEOT\ncat <<EOT | sh 2>\&1 | sed -E "s:^:  :"\n\1\nEOT/' \
-        | sed -E "s/^#([^\\\$\"\`'();{}-]*)$/echo \1/" \
+        | sed -E "s/^#([^\\\$\"\`'(){}<>;|&-]*)$/echo \1/" \
         | sed -E 's/^#(.*)$/cat <<"EOT"\n\1\nEOT/' >> "$test_script"
 
     sh "$test_script" > "$test_output"
