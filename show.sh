f87a2f28212d9d7def0fd81cd475e92e1d899685
Marcio <dev@marciof.com>
2017-03-19 12:23:09 -0700

    Add initial file plugin to experimental shell script version

--- show.sh
+++ show.sh
@@ -1,40 +1,45 @@
 #!/bin/sh
-set -e
+set -e -u
 
+program_path="$0"
 help_option=h
-plugins=dir
+exit_cant_execute=126
+plugins='file dir'
+plugin_description_dir='list directories via `ls`'
+plugin_description_file='read files'
 
-dir_plugin_name=dir
-dir_plugin_description='list directories via `ls`'
-
-dir_plugin_run() {
-    ls "$@"
-}
-
-var() {
-    eval echo "\$$1"
-}
-
-print_usage() {
-    printf "$(xargs -0 echo <<USAGE )" "$(basename "$0")" "$help_option"
-Usage: %s [OPTION]... [INPUT]...
-
-Options:
-  -%s            display this help and exit\n
-USAGE
-
-    if [ -n "$plugins" ]; then
-        printf "\nPlugins:\n"
-
-        for plugin in $plugins; do
-            printf "  %-14s%s\n" \
-                "$(var ${plugin}_plugin_name)" \
-                "$(var ${plugin}_plugin_description)"
-        done
+plugin_run_dir() {
+    if [ -d "$1" ]; then
+        ls -- "$1"
+    else
+        return $exit_cant_execute
     fi
 }
 
-parse_options() {
+plugin_run_file() {
+    if [ -e "$1" -a ! -d "$1" ]; then
+        cat -- "$1"
+    else
+        return $exit_cant_execute
+    fi
+}
+
+print_usage() {
+    cat <<USAGE
+Usage: $(basename "$program_path") [OPTION]... [INPUT]...
+
+Options:
+  -$help_option           display this help and exit
+
+Plugins:
+USAGE
+
+    for plugin in $plugins; do
+        printf "  %-13s%s\n" "$plugin" "$(var plugin_description_${plugin})"
+    done
+}
+
+process_options() {
     while getopts h option "$@"; do
         case "$option" in
             h)
@@ -42,13 +47,33 @@ parse_options() {
                 exit 0
                 ;;
             ?)
-                printf "Try '-%s' for more information.\n" "$help_option"
+                printf "Try '-%s' for more information.\n" "$help_option" 2>&1
                 exit 1
                 ;;
         esac
     done
 }
 
-parse_options "$@"
+var() {
+    eval echo "\$$1"
+}
+
+process_options "$@"
 shift $((OPTIND - 1))
-dir_plugin_run "$@"
+
+if [ $# -eq 0 ]; then
+    set -- .
+fi
+
+for input; do
+    for plugin in $plugins; do
+        if plugin_run_${plugin} "$input"; then
+            continue 2
+        fi
+    done
+
+    printf "%s: Unsupported input\n" "$input" 2>&1
+    exit 1
+done
+
+exit 0
