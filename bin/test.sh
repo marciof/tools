b65234f725af35b54aa8bd34237a2d975d1e46b8
Marcio <dev@marciof.com>
2017-03-19 08:12:12 -0700

    Save test script translation for debugging

--- bin/test.sh
+++ bin/test.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
+set -u
 
 for test_file; do
-    export TESTDIR="$(dirname "$test_file")"
+    test_dir="$(dirname "$test_file")"
+    test_script="$test_dir/$(basename "$test_file" .t).sh"
 
-    cat "$test_file" \
-        | sed -E '/^  /! s/^/#/' \
+    cat <<EOT > "$test_script"
+#!/bin/sh
+set -u
+TESTDIR="$(readlink -e "$test_dir")"
+EOT
+
+    chmod +x "$test_script"
+
+    sed -E '/^  /! s/^/#/' < "$test_file" \
         | sed -E 's/^  \$ //' \
         | grep -v -E '^  ' \
         | sed -E '/^#/! s/^(.+)$/cat <<"EOT"\n  $ \1\nEOT\ncat <<EOT | sh | sed -E "s:^:  :"\n\1\nEOT/' \
-        | sed -E 's/^#(.*)$/cat <<"EOT"\n\1\nEOT/'
+        | sed -E "s/^#([^\\\$\"'-]*)$/echo \1/" \
+        | sed -E 's/^#(.*)$/cat <<"EOT"\n\1\nEOT/' >> "$test_script"
+
+    echo "$test_script"
+    echo ==========
+    cat "$test_script"
+    echo ----------
 done
