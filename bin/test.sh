dacb4d5bc3d9b6dd55b992d8fb1fc2b51b379709
Marcio <dev@marciof.com>
2017-03-21 22:40:01 -0700

    Run each test in its own empty scratch directory

--- bin/test.sh
+++ bin/test.sh
@@ -6,8 +6,8 @@ help_option=h
 is_compile_only=0
 
 compile_t_to_sh() {
-    test_dir="$1"
-    eot_marker="EOT_$(head -c3 < /dev/urandom | od -An -tx1 | tr -d ' ')"
+    local test_dir="$1"
+    local eot_marker="EOT_$(head -c3 < /dev/urandom | od -An -tx1 | tr -d ' ')"
 
     cat <<EOT
 #!/bin/sh
@@ -54,16 +54,25 @@ process_options() {
 
 process_options "$@"
 shift $((OPTIND - 1))
+current_dir="$(pwd)"
 
 for test_file; do
-    test_dir="$(readlink -e "$(dirname "$test_file")")"
-    test_script="$test_dir/$(basename "$test_file" .t).sh"
-    test_output="$test_dir/$(basename "$test_file" .t).out"
+    test_dir="$(dirname "$test_file")"
+    abs_test_dir="$(readlink -e "$test_dir")"
+    test_script="$(basename "$test_file" .t).sh"
+    test_output="$(basename "$test_file" .t).err"
 
-    compile_t_to_sh "$test_dir" < "$test_file" > "$test_script"
+    compile_t_to_sh "$abs_test_dir" < "$test_file" > "$test_dir/$test_script"
+    chmod +x "$test_dir/$test_script"
 
-    if [ $is_compile_only -eq 0 ]; then
-        chmod +x "$test_script"
-        sh "$test_script" > "$test_output"
+    if [ "$is_compile_only" -eq 0 ]; then
+        test_scratch_dir="$(mktemp -d)"
+
+        cd "$test_scratch_dir"
+        sh "$abs_test_dir/$test_script" > "$abs_test_dir/$test_output"
+        cd "$current_dir"
+
+        rm -rf "$test_scratch_dir"
+        diff -u "$test_file" "$test_dir/$test_output" && rm "$test_dir/$test_script"
     fi
 done
