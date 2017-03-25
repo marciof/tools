adc96eb11be172a8dd51bc96aaf89891012a7743
Marcio <dev@marciof.com>
2017-03-25 11:30:03 -0700

    Forward signals to child pager process
    So that Ctrl-C and Ctrl-Z work as expected within `less`.

--- src/auto-pager.sh
+++ src/auto-pager.sh
@@ -2,7 +2,9 @@
 set -e -u
 
 buffer_file="$(mktemp)"
-trap "rm ${buffer_file}" EXIT
+trap 'rm "$buffer_file"' EXIT
+
+# FIXME: Using `head` stops `git` paging output abruptly.
 head -n "$(($(tput lines) / 2))" > "$buffer_file"
 
 if ! IFS= read -r line; then
@@ -12,11 +14,13 @@ fi
 
 pager_fifo="$(mktemp -u)"
 mkfifo "$pager_fifo"
-trap "rm ${pager_fifo}" EXIT
 
-less < "$pager_fifo" &
+pipe_to_pager_fifo() {
+    trap 'rm "$buffer_file" "$pager_fifo"' EXIT
+    cat "$buffer_file" > "$pager_fifo"
+    echo "$line" > "$pager_fifo"
+    cat > "$pager_fifo"
+}
 
-cat "$buffer_file" > "$pager_fifo"
-echo "$line" > "$pager_fifo"
-cat > "$pager_fifo"
-wait
+{ pipe_to_pager_fifo <&3 3<&- & } 3<&0
+exec less < "$pager_fifo"
