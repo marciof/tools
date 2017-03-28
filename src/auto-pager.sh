3802d82620ec617e09228851a2588099ede98efe
Marcio <dev@marciof.com>
2017-03-27 19:55:30 -0700

    Revert "Forward signals to child pager process"
    This reverts commit 1e3fdbfbd35d1aa422f2c3e0f37a055ac4659699.

--- src/auto-pager.sh
+++ src/auto-pager.sh
@@ -2,9 +2,7 @@
 set -e -u
 
 buffer_file="$(mktemp)"
-trap 'rm "$buffer_file"' EXIT
-
-# FIXME: Using `head` stops `git` paging output abruptly.
+trap "rm ${buffer_file}" EXIT
 head -n "$(($(tput lines) / 2))" > "$buffer_file"
 
 if ! IFS= read -r line; then
@@ -14,13 +12,11 @@ fi
 
 pager_fifo="$(mktemp -u)"
 mkfifo "$pager_fifo"
+trap "rm ${pager_fifo}" EXIT
 
-pipe_to_pager_fifo() {
-    trap 'rm "$buffer_file" "$pager_fifo"' EXIT
-    cat "$buffer_file" > "$pager_fifo"
-    echo "$line" > "$pager_fifo"
-    cat > "$pager_fifo"
-}
+less < "$pager_fifo" &
 
-{ pipe_to_pager_fifo <&3 3<&- & } 3<&0
-exec less < "$pager_fifo"
+cat "$buffer_file" > "$pager_fifo"
+echo "$line" > "$pager_fifo"
+cat > "$pager_fifo"
+wait
--- src/show.sh
+++ src/show.sh
@@ -127,10 +127,10 @@ Modes:
 USAGE
 
     for mode in stdin file dir vcs; do
-        if "mode_can_$mode"; then
-            unavailable=
-        else
+        if ! "mode_can_$mode"; then
             unavailable=' (UNAVAILABLE)'
+        else
+            unavailable=
         fi
 
         printf "  %-13s%s%s\n" \
