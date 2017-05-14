53fbe619ffb8adc6a9f78769a34b7d95957928e1
Marcio <dev@marciof.com>
2017-05-13 18:11:38 -0700

    Revert "Revert "Avoid `head` to not cause `git` to stop output abruptly""
    This reverts commit 457e1eedcdd6e419b7af6f474ec82b8e8ddc1309.

--- src/auto-pager.sh
+++ src/auto-pager.sh
@@ -4,8 +4,18 @@ set -e -u
 buffer_file="$(mktemp)"
 trap 'rm "$buffer_file"' EXIT
 
-# FIXME: Using `head` stops `git` paging output abruptly.
-head -n "$(($(tput lines) / 2))" > "$buffer_file"
+nr_buffered_lines=0
+max_nr_lines="$(($(tput lines) / 3))"
+
+# Can't use `head` here since it causes `git` to stop output abruptly.
+while IFS= read -r buffered_line; do
+    nr_buffered_lines=$((nr_buffered_lines + 1))
+    echo "$buffered_line" >>"$buffer_file"
+
+    if [ "$nr_buffered_lines" -ge "$max_nr_lines" ]; then
+        break
+    fi
+done
 
 if ! IFS= read -r line; then
     cat "$buffer_file"
@@ -22,5 +32,6 @@ pipe_to_pager_fifo() {
     cat > "$pager_fifo"
 }
 
+# Use `exec` on the pager to correctly receive signals.
 { pipe_to_pager_fifo <&3 3<&- & } 3<&0
 exec less < "$pager_fifo"
