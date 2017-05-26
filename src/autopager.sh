da9fc1ed792f040a7fb2dc656fc0327da627fac2
Marcio <dev@marciof.com>
2017-05-25 21:46:11 -0700

    Disable escape sequence interpretation in input

--- src/autopager.sh
+++ src/autopager.sh
@@ -10,7 +10,7 @@ max_nr_buffered_lines="$(($(tput lines) / 2))"
 # Can't use `head` here since it causes `git` to stop output abruptly.
 while IFS= read -r buffered_line; do
     nr_buffered_lines=$((nr_buffered_lines + 1))
-    echo "$buffered_line" >>"$buffer_file"
+    printf '%s\n' "$buffered_line" >>"$buffer_file"
 
     if [ "$nr_buffered_lines" -ge "$max_nr_buffered_lines" ]; then
         break
