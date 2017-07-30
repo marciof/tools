6094d6aba84afb4d87eea6047e6a64744beaf2d1
Marcio <dev@marciof.com>
2017-07-29 20:34:56 -0700

    Refactor into classes/methods for readability

--- sync.py
+++ sync.py
@@ -1,57 +1,80 @@
 #!/usr/bin/env python3
 
 # standard
+from abc import ABCMeta, abstractmethod
 from urllib.parse import parse_qs, urlparse
 import webbrowser
 
 # external
 import onedrivesdk
 
-api_base_url = 'https://api.onedrive.com/v1.0/'
-http_provider = onedrivesdk.HttpProvider()
+class Sync (metaclass = ABCMeta):
+    @abstractmethod
+    def authenticate(self):
+        pass
 
-# FIXME: check scopes
-auth_provider = onedrivesdk.AuthProvider(
-    http_provider = http_provider,
-    client_id = '8eaa14b1-642c-4085-a308-82cdc21e32eb',
-    scopes = ['wl.signin', 'wl.offline_access', 'onedrive.readwrite'])
+    @abstractmethod
+    def list_changes(self):
+        pass
 
-client = onedrivesdk.OneDriveClient(api_base_url, auth_provider, http_provider)
+class OneDriveSync (Sync):
 
-# FIXME: save session in proper folder
-try:
-    auth_provider.load_session()
-except FileNotFoundError as e:
-    # FIXME: GUI
-    print('Sync needs your permission to access your OneDrive.')
-    print('After logging in, copy and paste the final URL here.')
-    print('Press ENTER now to open the browser.')
-    input()
+    def __init__(self):
+        # FIXME: make configurable as default arguments
+        api_base_url = 'https://api.onedrive.com/v1.0/'
+        http_provider = onedrivesdk.HttpProvider()
 
-    redirect_url = 'https://login.microsoftonline.com/common/oauth2/nativeclient'
-    webbrowser.open(auth_provider.get_auth_url(redirect_url))
-    auth_url = input('Paste the final URL here and then press ENTER: ')
+        # FIXME: check scopes
+        self.auth_provider = onedrivesdk.AuthProvider(
+            http_provider = http_provider,
+            client_id = '8eaa14b1-642c-4085-a308-82cdc21e32eb',
+            scopes = ['wl.signin', 'wl.offline_access', 'onedrive.readwrite'])
 
-    # FIXME: handle parser errors
-    auth_code = parse_qs(urlparse(auth_url).query)['code']
+        self.client = onedrivesdk.OneDriveClient(
+            api_base_url, self.auth_provider, http_provider)
 
-    auth_provider.authenticate(auth_code, redirect_url, client_secret = None)
-    auth_provider.save_session()
-else:
-    # FIXME: how often to refresh?
-    auth_provider.refresh_token()
+    # FIXME: decouple from session save/load and save in proper folder
+    def authenticate(self):
+        try:
+            self.auth_provider.load_session()
+        except FileNotFoundError:
+            # FIXME: GUI
+            print('Sync needs your permission to access your OneDrive.')
+            print('After logging in, copy and paste the final URL here.')
+            print('Press ENTER now to open the browser.')
+            input()
 
-# FIXME: use logging (syslog?)
-print('Checking for changes')
+            redirect_url = 'https://login.microsoftonline.com/common/oauth2/nativeclient'
+            webbrowser.open(self.auth_provider.get_auth_url(redirect_url))
+            auth_url = input('Paste the final URL here and then press ENTER: ')
 
-# FIXME: persist token from last check
-token = None
+            # FIXME: handle parser errors
+            auth_code = parse_qs(urlparse(auth_url).query)['code']
 
-collection_page = client.item(id = 'root').delta(token).get()
+            self.auth_provider.authenticate(
+                auth_code, redirect_url, client_secret = None)
+            self.auth_provider.save_session()
+        else:
+            # FIXME: how often to refresh?
+            self.auth_provider.refresh_token()
 
-for item in collection_page:
-    print('-', item.name, item.parent_reference.path)
+    def list_changes(self):
+        # FIXME: use logging (syslog?)
+        print('Checking for changes')
 
-print('#', collection_page.token)
-print('#', collection_page.next_page_link)
-print('#', collection_page.delta_link)
+        # FIXME: persist token from last check
+        token = None
+
+        collection_page = self.client.item(id = 'root').delta(token).get()
+
+        for item in collection_page:
+            print('-', item.name, item.parent_reference.path)
+
+        print('#', collection_page.token)
+        print('#', collection_page.next_page_link)
+        print('#', collection_page.delta_link)
+
+if __name__ == "__main__":
+    sync = OneDriveSync()
+    sync.authenticate()
+    sync.list_changes()
