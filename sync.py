9086e5883fbe74b6d92d167ffdccf8a30664baf0
Marcio <dev@marciof.com>
2017-07-29 19:45:36 -0700

    Accept the final redirect URL instead of requiring just the auth code

--- sync.py
+++ sync.py
@@ -1,35 +1,41 @@
 #!/usr/bin/env python3
-# -*- coding: UTF-8 -*-
 
-# External
+# standard
+from urllib.parse import parse_qs, urlparse
+import webbrowser
+
+# external
 import onedrivesdk
 
-app_id = '8eaa14b1-642c-4085-a308-82cdc21e32eb'
 api_base_url = 'https://api.onedrive.com/v1.0/'
-
-# FIXME: first-time auth / access grant
-redirect_uri = 'https://login.microsoftonline.com/common/oauth2/nativeclient'
-
-# FIXME: check scopes
-scopes = ['wl.signin', 'wl.offline_access', 'onedrive.readwrite']
-
 http_provider = onedrivesdk.HttpProvider()
 
+# FIXME: check scopes
 auth_provider = onedrivesdk.AuthProvider(
     http_provider = http_provider,
-    client_id = app_id,
-    scopes = scopes)
+    client_id = '8eaa14b1-642c-4085-a308-82cdc21e32eb',
+    scopes = ['wl.signin', 'wl.offline_access', 'onedrive.readwrite'])
 
 client = onedrivesdk.OneDriveClient(api_base_url, auth_provider, http_provider)
-print('Authenticating')
 
 # FIXME: save session in proper folder
 try:
     auth_provider.load_session()
 except FileNotFoundError as e:
-    print(auth_provider.get_auth_url(redirect_uri))
-    auth_code = input('Paste code here: ')
-    auth_provider.authenticate(auth_code, redirect_uri, client_secret = None)
+    # FIXME: GUI
+    print('Sync needs your permission to access your OneDrive.')
+    print('After logging in, copy and paste the final URL here.')
+    print('Press ENTER now to open the browser.')
+    input()
+
+    redirect_url = 'https://login.microsoftonline.com/common/oauth2/nativeclient'
+    webbrowser.open(auth_provider.get_auth_url(redirect_url))
+    auth_url = input('Paste the final URL here and then press ENTER: ')
+
+    # FIXME: handle parser errors
+    auth_code = parse_qs(urlparse(auth_url).query)['code']
+
+    auth_provider.authenticate(auth_code, redirect_url, client_secret = None)
     auth_provider.save_session()
 else:
     # FIXME: how often to refresh?
