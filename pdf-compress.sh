ab0a13623eaa2c3aeef0c991a242488944798a93
Marcio <dev@marciof.com>
2020-08-16 20:00:16 -0700

    Cleanup script to create SMIL files for videos

---
+++ pdf-compress.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Compresses PDF files, without replacing the original.
+
+set -e -u
+
+if [ $# -eq 0 ]; then
+    set -- *.pdf
+fi
+
+for pdf_file; do
+    base_name="${pdf_file%.*}"
+    compressed_pdf_file="$base_name.pdf.compressed"
+
+    gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -sOutputFile="$compressed_pdf_file" "$pdf_file" >&2
+    printf '%s\t%s\n' "$pdf_file" "$compressed_pdf_file"
+done
