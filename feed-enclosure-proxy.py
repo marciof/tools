9654adc299c9567fc9f224e5f2d266f4dadf98f6
Marcio <dev@marciof.com>
2020-09-12 19:56:31 -0700

    Add note on youtube-dlc

--- feed-enclosure-proxy.py
+++ feed-enclosure-proxy.py
@@ -22,6 +22,7 @@ import feedparser # v5.2.1
 from flask import Flask, Response, request, redirect # v1.1.2
 import requests # v2.12.4
 from unidecode import unidecode # v1.1.1
+# TODO: use youtube-dlc?
 import youtube_dl # v2020.7.28
 
 
