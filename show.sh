b0704df078b9355be7388120d25323d3836e650d
Marcio <dev@marciof.com>
2020-09-24 23:51:12 -0700

    Update TODOs

--- show.sh
+++ show.sh
@@ -5,6 +5,7 @@
 # TODO: logging
 # TODO: measure performance
 # TODO: highlight output from `curl`
+# TODO: add option to pass flag "implicitly"? eg. `show.sh -i--stat @`
 # TODO: detect URLs without an explicit protocol? eg. "www."
 # TODO: piping (` | mode_run_color`) can remove error exit codes
 # TODO: use POSIX env vars `COLUMNS` and `LINES`?
