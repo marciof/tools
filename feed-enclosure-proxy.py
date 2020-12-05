ce1f6654f61aabeaa7b4611ecc56007e65e9fdac
Marcio <dev@marciof.com>
2020-12-05 15:09:22 -0800

    Upgrade youtube-dl

--- feed-enclosure-proxy.py
+++ feed-enclosure-proxy.py
@@ -23,7 +23,7 @@ from flask import Flask, Response, request, redirect # v1.1.2
 import requests # v2.12.4
 from unidecode import unidecode # v1.1.1
 # TODO: use youtube-dlc?
-import youtube_dl # v2020.7.28
+import youtube_dl # v2020.12.5
 
 
 app_name = 'Feed Enclosure Proxy'
