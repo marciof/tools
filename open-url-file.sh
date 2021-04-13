eaaa87809469930731564371f1a447b711616c73
Marcio <dev@marciof.com>
2021-04-13 22:37:39 +0100

    Document ".url" file handler script

--- open-url-file.sh
+++ open-url-file.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
-# File handler for the ".url" file format.
+
+# File handler for the ".url" file format (MS Windows 95 Internet shortcut),
+# passing the URL to `xdg-open`.
+#
+# Example:
 #   [InternetShortcut]
 #   URL=https://www.example.com
+#
+# Arguments: file
+# Stdin: none
+# Stdout: none
+#
+# Runtime dependencies:
+#   apt install xdg-utils # Version: 1.1.3-2ubuntu1.20.10.2 # open URL
+#
+# Test dependencies:
+#   apt install shellcheck # Version: 0.7.1-1build1
 
 set -e -u
 grep -m1 URL= <"$1" | sed -e 's/^URL\s*=\s*//' | xargs xdg-open
