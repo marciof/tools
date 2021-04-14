128fdcec61b4b954ed63bcb32c1dfdfbb4c8107e
Marcio <dev@marciof.com>
2021-04-14 17:10:00 +0100

    Use `subTest` for test parameterization and add a generator transform version

--- puzzles/alternate_matrix.py
+++ puzzles/alternate_matrix.py
@@ -2,17 +2,19 @@
 # -*- coding: UTF-8 -*-
 
 """
-Read the rows of a matrix (as a 2D array) alternatively from right to left,
-left to right, and so on, and return them as a 1D array.
+Read the rows of a matrix (as a 2D array) alternatively from left to right,
+right to left, and so on, and return them as a 1D array.
 
 Time: O(n)
 """
 
+from typing import List
 import unittest
 
-def transform_simple(matrix):
-    is_reversed = False
+
+def transform_simple(matrix: List[List]) -> List:
     array = []
+    is_reversed = False
 
     for row in matrix:
         array.extend(reversed(row) if is_reversed else row)
@@ -20,7 +22,20 @@ def transform_simple(matrix):
 
     return array
 
-def transform_manual(matrix):
+
+def transform_iter(matrix: List[List]) -> List:
+    is_reversed = False
+
+    for row in matrix:
+        if is_reversed:
+            yield from reversed(row)
+        else:
+            yield from row
+
+        is_reversed = not is_reversed
+
+
+def transform_manual(matrix: List[List]) -> List:
     is_reversed = False
     array = []
 
@@ -41,39 +56,49 @@ def transform_manual(matrix):
 
     return array
 
-class TestSuite:
+
+class Test (unittest.TestCase):
+    transform_iter_list = lambda matrix: list(transform_iter(matrix))
+
+    transform_impls = (
+        transform_simple,
+        transform_manual,
+        transform_iter_list,
+    )
+
     def test_square_matrix(self):
-        self.assertEqual(
-            self.transform([
-                [1, 2, 3],
-                [4, 5, 6],
-                [7, 8, 9]
-            ]),
-            [1, 2, 3, 6, 5, 4, 7, 8, 9])
+        for transform in self.transform_impls:
+            with self.subTest(transform):
+                self.assertEqual(
+                    transform([
+                        [1, 2, 3],
+                        [4, 5, 6],
+                        [7, 8, 9]
+                    ]),
+                    [1, 2, 3, 6, 5, 4, 7, 8, 9])
 
     def test_rectangular_matrix(self):
-        self.assertEqual(
-            self.transform([
-                ['a', 'b'],
-                ['c', 'd'],
-                ['e', 'f'],
-                ['g', 'h'],
-            ]),
-            ['a', 'b', 'd', 'c', 'e', 'f', 'h', 'g'])
+        for transform in self.transform_impls:
+            with self.subTest(transform):
+                self.assertEqual(
+                    transform([
+                        ['a', 'b'],
+                        ['c', 'd'],
+                        ['e', 'f'],
+                        ['g', 'h'],
+                    ]),
+                    ['a', 'b', 'd', 'c', 'e', 'f', 'h', 'g'])
 
     def test_single_element_matrix(self):
-        self.assertEqual(self.transform([[100]]), [100])
+        for transform in self.transform_impls:
+            with self.subTest(transform):
+                self.assertEqual(transform([[100]]), [100])
 
     def test_empty_matrix(self):
-        self.assertEqual(self.transform([]), [])
+        for transform in self.transform_impls:
+            with self.subTest(transform):
+                self.assertEqual(transform([]), [])
 
-class TestSimple (unittest.TestCase, TestSuite):
-    def transform(self, *args):
-        return transform_simple(*args)
-
-class TestManual (unittest.TestCase, TestSuite):
-    def transform(self, *args):
-        return transform_manual(*args)
 
 if __name__ == '__main__':
     unittest.main(verbosity = 2)
