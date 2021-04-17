128bc818a3a7e283964f0d435259b03769862e16
Marcio <dev@marciof.com>
2021-04-17 10:12:32 +0100

    Add balanced parenthesis puzzle

---
+++ puzzles/balanced_parens.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""
+Check if a string contains only balanced parenthesis.
+"""
+
+import unittest
+
+
+def is_balanced(string: str) -> bool:
+    """
+    Time: O(n)
+    Space: O(1)
+    """
+
+    depth = 0
+
+    for paren in string:
+        if paren == '(':
+            depth += 1
+        elif paren == ')':
+            depth -= 1
+            if depth < 0:
+                return False
+        else:
+            return False
+
+    return depth == 0
+
+
+class Test (unittest.TestCase):
+    def test_balanced(self):
+        cases = [
+            '',
+            '()',
+            '(())(())',
+            '()()()',
+            '(()())',
+            '((()))',
+        ]
+
+        for case in cases:
+            self.assertTrue(is_balanced(case),
+                'Balanced parenthesis: %s' % case)
+
+    def test_unbalanced(self):
+        cases = [
+            '(',
+            ')',
+            '(()',
+            ')()',
+            '())',
+            '()(',
+        ]
+
+        for case in cases:
+            self.assertFalse(is_balanced(case),
+                'Non-balanced parenthesis: %s' % case)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity = 2)
