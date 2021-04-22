7eb5b1261486b762034fdfd2bb7716b397d9344c
Marcio <dev@marciof.com>
2021-04-22 21:48:50 +0100

    Add option to control youtube-dl's video format
    Also useful for debugging.

--- feed-enclosure/download_enclosure.sh
+++ feed-enclosure/download_enclosure.sh
@@ -5,7 +5,7 @@
 #
 # Runtime dependencies:
 #   ./youtube_dl_wrapper.py
-#   apt install uget # Version: 2.2.3-2 # download non-YouTube URLs
+#   apt install uget # Version: 2.2.3-2 # downloader tool
 #   apt install ffmpeg # Version: 7:4.3.1-4ubuntu1 # merge video/audio
 #
 # Test dependencies:
@@ -17,8 +17,11 @@ YOUTUBE_DL_BIN="${YOUTUBE_DL_BIN:-$(dirname "$(readlink -e "$0")")/youtube_dl_wr
 UGET_BIN="${UGET_BIN:-uget-gtk}"
 
 help_opt=h
-folder_opt=f
-folder=
+download_folder_opt=f
+ytdl_video_format_opt=y
+
+download_folder=.
+ytdl_video_format=bestvideo+bestaudio
 
 check_dependencies() {
     if ! command -v "$YOUTUBE_DL_BIN" >/dev/null; then
@@ -36,7 +39,7 @@ is_ign_daily_fix_url() {
     printf %s "$1" | grep -q -P '://assets\d*\.ign\.com/videos/'
 }
 
-upgrade_ign_daily_fix_url_res() {
+upgrade_ign_daily_fix_url_video_res() {
     ign_width='[[:digit:]]+'
     ign_hash='[[:xdigit:]]+'
     ign_bitrate='[[:digit:]]+'
@@ -82,19 +85,23 @@ print_usage() {
     cat <<EOT >&2
 Usage: $(basename "$0") [OPTION]... URL
 
-Note: if the URL has a URL fragment then it's an optional filename hint
+Note: if the URL contains a fragment part then it's an optional filename hint
 
 Options:
   -$help_opt           display this help and exit
-  -$folder_opt PATH      download save location to "PATH"
+  -$download_folder_opt PATH      download save location to "PATH", defaults to "$download_folder"
+  -$ytdl_video_format_opt FORMAT    set youtube-dl video "FORMAT", defaults to "$ytdl_video_format"
 EOT
 }
 
 process_options() {
-    while getopts "$folder_opt:$help_opt" process_opt "$@"; do
+    while getopts "$download_folder_opt:$ytdl_video_format_opt:$help_opt" process_opt "$@"; do
         case "$process_opt" in
-            "$folder_opt")
-                folder="$OPTARG"
+            "$download_folder_opt")
+                download_folder="$OPTARG"
+                ;;
+            "$ytdl_video_format_opt")
+                ytdl_video_format="$OPTARG"
                 ;;
             "$help_opt")
                 print_usage
@@ -106,17 +113,12 @@ process_options() {
                 ;;
         esac
     done
-
-    if [ -z "$folder" ]; then
-        folder=.
-    fi
 }
 
 # TODO: GUI notification of download errors or significant events?
 #       eg. ffmpeg muxing start/end, error "downloading" livestreams, etc
 main() {
     check_dependencies
-
     process_options "$@"
     shift $((OPTIND - 1))
 
@@ -133,19 +135,20 @@ main() {
         # TODO: add IGN Daily Fix support to youtube-dl?
         #       https://github.com/ytdl-org/youtube-dl/tree/master#adding-support-for-a-new-site
         #       https://github.com/ytdl-org/youtube-dl/issues/24771
-        download_via_uget "$(upgrade_ign_daily_fix_url_res "$url")" "$folder"
+        download_via_uget \
+            "$(upgrade_ign_daily_fix_url_video_res "$url")" \
+            "$download_folder"
     else
         (
             # FIXME: youtube-dl doesn't have an option for the output directory,
             #        so as a workaround go to where it should be downloaded
-            cd -- "$folder"
+            cd -- "$download_folder"
 
-            # TODO: getopt option to control video quality?
             "$YOUTUBE_DL_BIN" \
                 --verbose \
                 --external-downloader uget \
                 --add-metadata \
-                --format bestvideo+bestaudio \
+                --format "$ytdl_video_format" \
                 -- \
                 "$url"
         )
