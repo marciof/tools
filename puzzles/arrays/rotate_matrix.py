bcea83b2d5376d47463143c8bdc092b5888522c3
Marcio <dev@marciof.com>
2021-05-01 17:27:23 +0100

    Save up work in progress, even though it's broken for future reference

--- puzzles/arrays/rotate_matrix.py
+++ puzzles/arrays/rotate_matrix.py
@@ -32,14 +32,72 @@ def rotate_matrix(matrix: List[List]) -> List[List]:
     return new_matrix
 
 
+ROW_KIND = 0
+COL_KIND = 1
+
+RIGHT_DIR = [0, +1]
+LEFT_DIR = [0, -1]
+DOWN_DIR = [+1, 0]
+UP_DIR = [-1, 0]
+
+
+def should_change_direction(row_idx, col_idx, side_length) -> bool:
+    max_idx = side_length - 1
+
+    if (row_idx == 0) and (col_idx == max_idx):
+        return True
+
+    if (row_idx == max_idx) and (col_idx == max_idx):
+        return True
+
+    if (row_idx == max_idx) and (col_idx == 0):
+        return True
+
+    if (row_idx == 0) and (col_idx == 0):
+        return True
+
+    return False
+
+
 def rotate_matrix_in_place(matrix: List[List]) -> List[List]:
     """
     Time: O(n), where n=number of elements in matrix
     Space: O(1)
     """
 
-    if len(matrix) <= 1:
+    side_length = len(matrix)
+
+    if side_length <= 1:
         return matrix
+
+    directions = [RIGHT_DIR, DOWN_DIR, LEFT_DIR, UP_DIR]
+
+    src_dir_idx = directions.index(RIGHT_DIR)
+    src_row_idx = 0
+    src_col_idx = 0
+
+    dest_dir_idx = directions.index(DOWN_DIR)
+    dest_row_idx = 0
+    dest_col_idx = side_length - 1
+
+    for step in range(2 * side_length + 2 * (side_length - 2)):
+        src_value = matrix[src_row_idx][src_col_idx]
+        dest_value = matrix[dest_row_idx][dest_col_idx]
+
+        print(src_value, dest_value)
+
+        src_dir = directions[src_dir_idx]
+        src_row_idx += src_dir[ROW_KIND]
+        src_col_idx += src_dir[COL_KIND]
+
+        dest_dir = directions[dest_dir_idx]
+        dest_row_idx += dest_dir[ROW_KIND]
+        dest_col_idx += dest_dir[COL_KIND]
+
+        if should_change_direction(src_row_idx, src_col_idx, side_length):
+            src_dir_idx = (src_dir_idx + 1) % len(directions)
+        if should_change_direction(dest_row_idx, dest_col_idx, side_length):
+            dest_dir_idx = (dest_dir_idx + 1) % len(directions)
 
     return []
 
@@ -47,7 +105,7 @@ def rotate_matrix_in_place(matrix: List[List]) -> List[List]:
 class Test (unittest.TestCase):
     rotate_impls = {
         rotate_matrix,
-        rotate_matrix_in_place,
+        # rotate_matrix_in_place,
     }
 
     def test_empty_matrix(self):
@@ -90,4 +148,9 @@ class Test (unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    # unittest.main(verbosity=2)
+    rotate_matrix_in_place([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ])
