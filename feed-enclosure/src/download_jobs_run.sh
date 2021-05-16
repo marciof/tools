75f2af1cc553e097ad1ef32896c1444676366cda
Marcio <dev@marciof.com>
2021-05-15 22:47:36 -0700

    Run pending download jobs in parallel

--- feed-enclosure/src/download_jobs_run.sh
+++ feed-enclosure/src/download_jobs_run.sh
@@ -11,10 +11,11 @@ ENCLOSURE_DL_BIN="${ENCLOSURE_DL_BIN:-$(dirname "$(readlink -e "$0")")/enclosure
 RECSEL_BIN="${RECSEL_BIN:-recsel}"
 
 jobs_db="$("$DL_JOB_FIND_DB_BIN")"
+num_jobs="$("$RECSEL_BIN" -c "$jobs_db")"
+job_num=0
 
-# TODO don't run download jobs serially
-while true; do
-    job="$("$RECSEL_BIN" -n 0 "$jobs_db")"
+while [ "$job_num" -lt "$num_jobs" ]; do
+    job="$("$RECSEL_BIN" -n "$((num_jobs - job_num - 1))" "$jobs_db")"
 
     if [ -z "$job" ]; then
         break
@@ -22,11 +23,19 @@ while true; do
 
     echo "$job"
 
-    url="$(printf %s "$job" | "$RECSEL_BIN" -P URL)"
-    format="$(printf %s "$job" | "$RECSEL_BIN" -P Format)"
-    folder="$(printf %s "$job" | "$RECSEL_BIN" -P Folder)"
+    url="$(echo "$job" | "$RECSEL_BIN" -P URL)"
+    format="$(echo "$job" | "$RECSEL_BIN" -P Format)"
+    folder="$(echo "$job" | "$RECSEL_BIN" -P Folder)"
 
-    # TODO use generic download format option
-    "$ENCLOSURE_DL_BIN" -y "$format" -f "$folder" -- "$url"
-    "$DL_OB_DELETE_BIN" "$url" "$format" "$folder"
+    {
+        # TODO use generic download format option
+        "$ENCLOSURE_DL_BIN" -y "$format" -f "$folder" -- "$url"
+        "$DL_OB_DELETE_BIN" "$url" "$format" "$folder"
+    }&
+
+    job_num="$((job_num + 1))"
+    # TODO option for sleep duration?
+    sleep 1
 done
+
+wait
