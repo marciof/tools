15e5e088b029b8f6d9a9e0d88955aeb61bf761bc
Marcio <dev@marciof.com>
2021-05-16 23:28:30 -0700

    Document the script that creates download jobs

--- feed-enclosure/src/download_jobs_create.sh
+++ feed-enclosure/src/download_jobs_create.sh
@@ -1,30 +1,45 @@
 #!/bin/sh
 
-# TODO document
+# Adds a job to the download jobs recfile database.
+#
+# Runtime dependencies:
+#   ./download_jobs_find_db.sh
+#   apt install recutils # Version: 1.8-1
+#
+# Test dependencies:
+#   ../tst/lint_shell.sh
 
 set -e -u
 
-# TODO check dependencies
 DL_JOB_FIND_DB_BIN="${DL_JOB_FIND_DB_BIN:-$(dirname "$(readlink -e "$0")")/download_jobs_find_db.sh}"
 RECINS_BIN="${RECINS_BIN:-recins}"
+
+if ! command -v "$DL_JOB_FIND_DB_BIN" >/dev/null; then
+    echo "Error: $DL_JOB_FIND_DB_BIN not found (override \$DL_JOB_FIND_DB_BIN)" >&2
+    exit 1
+fi
+
+if ! command -v "$RECINS_BIN" >/dev/null; then
+    echo "Error: $RECINS_BIN not found (override \$RECINS_BIN)" >&2
+    exit 1
+fi
 
 if [ $# -ne 3 ]; then
     echo "Usage: $(basename "$0") URL FORMAT FOLDER" >&2
     exit 1
 fi
 
-jobs_db="$("$DL_JOB_FIND_DB_BIN")"
-
 url="$1"
 format="$2"
 folder="$3"
 shift 3
 
+jobs_db="$("$DL_JOB_FIND_DB_BIN")"
+
 mkdir -p "$(dirname "$jobs_db")"
 touch "$jobs_db"
 
 # TODO detect duplicates?
-# TODO add ID/hash for easier deletion?
 "$RECINS_BIN" \
     -f URL -v "$url" \
     -f Format -v "$format" \
