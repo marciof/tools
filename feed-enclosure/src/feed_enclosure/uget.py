e79a5639b4b3e386ec48aff69044606bb8d085de
Marcio <dev@marciof.com>
2021-05-21 10:35:23 -0700

    Initial refactoring of uGet specific code out to its own module
    For readability and reuse.

---
+++ feed-enclosure/src/feed_enclosure/uget.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""
+Wraps uGet to add additional functionality and workaround certain issues.
+"""
+
+# stdlib
+import os
+import os.path
+import subprocess
+from typing import Callable, List, Optional, Tuple
+
+# external
+# FIXME missing type stubs for some external libraries
+from asyncinotify import Inotify, Mask  # type: ignore
+from unidecode import unidecode
+
+
+class Uget:
+
+    # FIXME uGet doesn't handle filenames with Unicode characters on the CLI
+    def clean_file_name(self, file_name: str) -> str:
+        return unidecode(file_name)
+
+    def ensure_running(self, executable_name: str) -> None:
+        """
+        If uGet isn't already running, then starting it up will block execution
+        until it exits. To avoid that, ensure it's always running already in
+        the background.
+        """
+
+        # TODO detect existence of `start_new_session` (it's POSIX specific)
+        subprocess.Popen([executable_name, '--quiet'],
+                         start_new_session=True,
+                         stdout=subprocess.DEVNULL,
+                         stderr=subprocess.DEVNULL)
+
+    def get_file_disk_sizes(
+            self, path: str, block_size_bytes: int = 512) -> Tuple[int, int]:
+        """
+        Get the reported file size, as well as the actual total block size
+        on disk.
+        """
+
+        stat = os.stat(path)
+
+        # TODO detect availability of `st_blocks` (it's Unix specific)
+        block_size = stat.st_blocks * block_size_bytes
+
+        return (stat.st_size, block_size)
+
+    def is_downloaded(
+            self,
+            file_name: str,
+            file_size: Optional[int],
+            progress_cb: Optional[Callable[[int], None]] = None) -> bool:
+
+        # If the file to be downloaded has been already reserved
+        # space on disk, then its apparent size will already be the
+        # final size. In that case use the disk block size to get
+        # an idea for when its download is complete.
+        (actual_size, block_size) = self.get_file_disk_sizes(file_name)
+
+        if progress_cb is not None:
+            progress_cb(block_size)
+
+        return ((block_size >= actual_size)
+                and ((file_size is None) or (actual_size == file_size)))
+
+    def make_command(
+            self,
+            executable_name: str,
+            url: str,
+            file_name: Optional[str] = None,
+            http_user_agent: Optional[str] = None) -> List[str]:
+
+        command = [executable_name, '--quiet']
+
+        if file_name is not None:
+            # TODO make folder path absolute for uGet
+            (folder, filename) = self.split_folder_file_name(file_name)
+            command += ['--filename=' + filename, '--folder=' + folder]
+
+        if http_user_agent is not None:
+            command += ['--http-user-agent=' + http_user_agent]
+
+        return command + ['--', url]
+
+    def split_folder_file_name(self, path: str) -> Tuple[str, str]:
+        (folder, file_name) = os.path.split(path)
+        return (folder or os.getcwd(), file_name)
+
+    async def wait_for_download(
+            self,
+            file_name: str,
+            file_size: Optional[int],
+            progress_cb: Optional[Callable[[int], None]] = None) -> None:
+
+        (folder, filename) = self.split_folder_file_name(file_name)
+
+        # TODO use the `watchdog` package to be platform agnostic?
+        with Inotify() as inotify:
+            # TODO watch target file only for performance (measure first)
+            inotify.add_watch(folder, Mask.ONLYDIR | Mask.CLOSE | Mask.CREATE
+                              | Mask.MODIFY | Mask.MOVED_TO)
+
+            # TODO add an optional check, if after starting uGet there's no
+            #      inotify event for the target filename then it's
+            #      suspicious and flag it?
+            async for event in inotify:
+                if event.path.name != file_name:
+                    continue
+                if self.is_downloaded(file_name, file_size, progress_cb):
+                    break
--- feed-enclosure/src/feed_enclosure/youtube_dl.py
+++ feed-enclosure/src/feed_enclosure/youtube_dl.py
@@ -7,46 +7,40 @@ Wraps youtube-dl to add support for uGet as an external downloader.
 
 # stdlib
 import asyncio
-import os
-import os.path
-import subprocess
 from time import time
-from typing import List, Optional, Tuple, Type
+from typing import List, Optional, Type
 
 # external
 # FIXME missing type stubs for some external libraries
-from asyncinotify import Inotify, Mask  # type: ignore
-from unidecode import unidecode
 import youtube_dl  # type: ignore
 from youtube_dl.downloader.external import _BY_NAME, ExternalFD  # type: ignore
 
+# internal
+from . import uget
 
-# TODO refactor out to its own module, separate from this wrapper script
+
 class UgetFD(ExternalFD):
     """
     https://github.com/ytdl-org/youtube-dl#mandatory-and-optional-metafields
     """
 
     AVAILABLE_OPT = '--version'
-    DOWNLOAD_PROGRESS_THROTTLE_INTERVAL_SEC = 0.5
+
+    def __init__(
+            self,
+            *args,
+            download_progress_throttle_interval_sec: float = 0.5,
+            **kwargs):
+
+        super().__init__(*args, **kwargs)
+        self.uget = uget.Uget()
+        self.last_timestamp = time()
+        self.download_progress_throttle_interval_sec = \
+            download_progress_throttle_interval_sec
 
     @classmethod
     def get_basename(cls) -> str:
         return 'uget-gtk'
-
-    @classmethod
-    def ensure_running(cls) -> None:
-        """
-        If uGet isn't already running, then starting it up will block execution
-        until it exits. To avoid that, ensure it's always running already in
-        the background.
-        """
-
-        # TODO detect existence of `start_new_session` (it's POSIX specific)
-        subprocess.Popen([cls.get_basename(), '--quiet'],
-                         start_new_session=True,
-                         stdout=subprocess.DEVNULL,
-                         stderr=subprocess.DEVNULL)
 
     def error(self, message: str, *args) -> None:
         self.report_error(('[%s] ' + message) % (self.get_basename(), *args))
@@ -57,82 +51,39 @@ class UgetFD(ExternalFD):
     def warn(self, message: str, *args) -> None:
         self.report_warning(('[%s] ' + message) % (self.get_basename(), *args))
 
-    # FIXME uGet misinterprets relative folder paths, so make it absolute
-    def split_folder_filename(self, path: str) -> Tuple[str, str]:
-        (folder, filename) = os.path.split(path)
-        return (folder or os.getcwd(), filename)
+    def log_progress_throttled(
+            self, current_size: int, expected_size: Optional[int]):
 
-    def get_disk_sizes(
-            self, path: str, block_size_bytes: int = 512) -> Tuple[int, int]:
-        """
-        Get the reported file size, as well as the actual total block size
-        on disk.
-        """
+        timestamp = time()
+        should_log = ((timestamp - self.last_timestamp)
+                      >= self.download_progress_throttle_interval_sec)
 
-        stat = os.stat(path)
+        if should_log:
+            self.last_timestamp = timestamp
+            self.info('Downloaded %s block bytes (%s)',
+                      current_size,
+                      self.calc_format_percent(current_size, expected_size))
 
-        # TODO detect availability of `st_blocks` (it's Unix specific)
-        block_size = stat.st_blocks * block_size_bytes
-
-        return (stat.st_size, block_size)
-
-    # FIXME uGet doesn't handle filenames with Unicode characters on the CLI
     def temp_name(self, filename: str) -> str:
-        non_unicode_filename = unidecode(filename)
+        clean_file_name = self.uget.clean_file_name(filename)
 
-        if non_unicode_filename != filename:
-            self.warn('Filename contains Unicode, cleaning up: %s',
-                      non_unicode_filename)
+        if clean_file_name != filename:
+            self.warn('Filename cleaned up: %s', clean_file_name)
 
-        return super().temp_name(non_unicode_filename)
+        return super().temp_name(clean_file_name)
 
     def calc_format_percent(self, count: int, total: Optional[int]) -> str:
         return self.format_percent(self.calc_percent(count, total)).strip()
 
-    def is_downloaded(
-            self,
-            tmpfilename: str,
-            info_dict: dict,
-            should_log_progress: bool = False) -> bool:
-
-        # TODO use `filesize_approx` as well?
-        expected_size = info_dict.get('filesize')
-
-        # If the file to be downloaded has been already reserved
-        # space on disk, then its apparent size will already be the
-        # final size. In that case use the disk block size to get
-        # an idea for when its download is complete.
-        (size, block_size) = self.get_disk_sizes(tmpfilename)
-
-        if should_log_progress:
-            self.info('Downloaded %s block bytes (%s)',
-                      block_size,
-                      self.calc_format_percent(block_size, expected_size))
-
-        is_downloaded = ((block_size >= size)
-                         and ((expected_size is None)
-                              or (size == expected_size)))
-
-        return is_downloaded
-
     def _make_cmd(self, tmpfilename: str, info_dict: dict) -> List[str]:
-        (folder, filename) = self.split_folder_filename(tmpfilename)
-
         # TODO honor youtube-dl's proxy option/value
         # TODO honor `external_downloader_args`
-        cmd = [
+        return self.uget.make_command(
             self.get_basename(),
-            '--quiet',
-            '--filename=' + filename,
-            '--folder=' + folder,
-        ]
-
-        user_agent = info_dict.get('http_headers', {}).get('User-Agent')
-
-        if user_agent is not None:
-            cmd += ['--http-user-agent=' + user_agent]
-
-        return cmd + ['--', info_dict['url']]
+            info_dict['url'],
+            file_name=tmpfilename,
+            http_user_agent=info_dict.get('http_headers', {})
+                .get('User-Agent'))
 
     def _call_downloader(self, tmpfilename: str, info_dict: dict) -> int:
         # TODO use `filesize_approx` as well?
@@ -144,7 +95,7 @@ class UgetFD(ExternalFD):
             self.info('Expected file size: %d bytes', expected_size)
 
         try:
-            is_downloaded = self.is_downloaded(tmpfilename, info_dict)
+            is_downloaded = self.uget.is_downloaded(tmpfilename, expected_size)
             return_code: Optional[int] = 0
         except FileNotFoundError:
             is_downloaded = False
@@ -156,7 +107,7 @@ class UgetFD(ExternalFD):
             return 0
 
         if return_code is None:
-            self.ensure_running()
+            self.uget.ensure_running(self.get_basename())
             return_code = super()._call_downloader(tmpfilename, info_dict)
             self.info('Return code: %s', return_code)
         else:
@@ -166,42 +117,13 @@ class UgetFD(ExternalFD):
         return return_code
 
     async def wait_for_download(self, tmpfilename: str, info_dict: dict):
-        (folder, filename) = self.split_folder_filename(tmpfilename)
-        self.info('Starting inotify watch on folder: %s', folder)
+        expected_size = info_dict.get('filesize')
 
-        # TODO use the `watchdog` package to be platform agnostic?
-        with Inotify() as inotify:
-            # TODO watch target file only for performance (measure first)
-            inotify.add_watch(folder, Mask.ONLYDIR | Mask.CLOSE | Mask.CREATE
-                              | Mask.MODIFY | Mask.MOVED_TO)
-
-            event_count = 0
-            event_skipped_count = 0
-            last_timestamp = time()
-
-            # TODO add an optional check, if after starting uGet there's no
-            #      inotify event for the target filename then it's
-            #      suspicious and flag it?
-            async for event in inotify:
-                event_count += 1
-
-                if event.path.name != tmpfilename:
-                    event_skipped_count += 1
-                    continue
-
-                timestamp = time()
-                should_log = ((timestamp - last_timestamp)
-                              >= self.DOWNLOAD_PROGRESS_THROTTLE_INTERVAL_SEC)
-
-                if self.is_downloaded(tmpfilename, info_dict, should_log):
-                    break
-                if should_log:
-                    last_timestamp = timestamp
-
-            self.info('inotify events: %s skipped / %s total (%s)',
-                      event_skipped_count, event_count,
-                      self.calc_format_percent(
-                          event_skipped_count, event_count))
+        await self.uget.wait_for_download(
+            tmpfilename,
+            expected_size,
+            lambda current_size:
+                self.log_progress_throttled(current_size, expected_size))
 
 
 def register_external_downloader(name: str, klass: Type[ExternalFD]) -> None:
