3f43e0fda018aa7779ebef7f931b28113e261658
Marcio <dev@marciof.com>
2021-05-21 21:20:53 -0700

    Add missing export of PYTHONPATH

--- feed-enclosure/src/liferea_rebuild_feed_filtercmd.sh
+++ feed-enclosure/src/liferea_rebuild_feed_filtercmd.sh
@@ -2,9 +2,12 @@
 
 # Wraps `feed_enclosure.feed_rebuilder` for use with Liferea.
 
+# TODO add catchall logging wrapper?
+
 set -e -u
 
 PYTHONPATH="${PYTHONPATH:-}:$(dirname "$(readlink -e "$0")")"
 PYTHON3="${PYTHON3:-python3}"
 
+export PYTHONPATH
 "$PYTHON3" -m feed_enclosure.feed_rebuilder "$@"
