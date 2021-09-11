8ec49cc9e922a94b9103bc5e1dbacdd301caf408
Marcio <dev@marciof.com>
2021-09-11 15:05:26 -0700

    Use libxdo to minimize Liferea until Xlib can be used

--- feed-enclosure/feed_enclosure/xlib.py
+++ feed-enclosure/feed_enclosure/xlib.py
@@ -9,6 +9,7 @@ from typing import Iterator, Optional
 
 # external
 # FIXME missing type stubs for some external libraries
+from xdo import Xdo  # type: ignore
 from Xlib import X, Xutil  # type: ignore
 from Xlib.display import Display  # type: ignore
 from Xlib.protocol.event import ClientMessage  # type: ignore
@@ -23,6 +24,7 @@ class Xlib:
 
     def __init__(self, display: Optional[Display] = None):
         self.logger = log.create_logger('xlib')
+        self.xdo = Xdo()
 
         self.display = display or Display()
         self.logger.debug('Display: %s', self.display)
@@ -30,6 +32,7 @@ class Xlib:
         self.root_window = self.display.screen().root
         self.logger.debug('Root window: %s', self.root_window)
 
+    # TODO filter to visible windows only
     def iter_windows(
             self, instance_name: str, class_name: str) \
             -> Iterator[Window]:
@@ -41,10 +44,13 @@ class Xlib:
                 self.logger.debug('Matched window: %s', window)
                 yield window
 
-    # TODO iconify window is currently broken (use python-libxdo?)
+    # TODO fix non-working Xlib iconify window
     def iconify_window(self, window: Window) -> None:
-        # https://tronche.com/gui/x/xlib/ICC/client-to-window-manager/XIconifyWindow.html
-        # https://babbage.cs.qc.cuny.edu/courses/GUIDesign/motif-faq.html
+        """
+        Manual: https://tronche.com/gui/x/xlib/ICC/client-to-window-manager/XIconifyWindow.html
+        Example: https://babbage.cs.qc.cuny.edu/courses/GUIDesign/motif-faq.html#260
+        # noqa
+        """
 
         iconic_state_message = ClientMessage(
             window=window,
@@ -58,9 +64,15 @@ class Xlib:
             event=iconic_state_message,
             event_mask=X.SubstructureNotifyMask | X.SubstructureRedirectMask)
 
+    # TODO remove dependency on `libxdo`
     def iconify_windows(self, instance_name: str, class_name: str) -> None:
-        for window in self.iter_windows(instance_name, class_name):
-            self.iconify_window(window)
+        window_ids = self.xdo.search_windows(
+            winclassname=class_name.encode(),
+            winname=instance_name.encode(),
+            only_visible=True)
+
+        for window_id in window_ids:
+            self.xdo.minimize_window(window_id)
 
     def has_window(self, instance_name: str, class_name: str) -> bool:
         for _ in self.iter_windows(instance_name, class_name):
