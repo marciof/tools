14c520a73a31d6be21bf22908647ec94117bea5b
Marcio <dev@marciof.com>
2021-09-13 19:30:10 -0700

    Convert Python style checking shell script to a Python script

--- feed-enclosure/tests/lint.py
+++ feed-enclosure/tests/lint.py
@@ -1,18 +1,19 @@
-#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
 # stdlib
 import os
 import sys
-from typing import Any
+from typing import Optional, Tuple
 
 # external
+# FIXME missing type stubs for some external libraries
 from mypy.main import main as mypy_main
+from pycodestyle import StyleGuide, StandardReport  # type: ignore
 
 
-def main() -> Any:
+def main() -> Optional[Tuple[Optional[SystemExit], StandardReport]]:
     root_path = os.path.dirname(os.path.dirname(__file__))
-    mypy_exit_code = None
+    mypy_exit_error = None
 
     try:
         print('--- Mypy ---')
@@ -23,9 +24,21 @@ def main() -> Any:
             stderr=sys.stderr,
             args=['--', root_path])
     except SystemExit as error:
-        mypy_exit_code = error.code
+        mypy_exit_error = error
 
-    return mypy_exit_code
+    print()
+
+    print('--- pycodestyle ---')
+    style_report = StyleGuide().check_files([root_path])
+
+    if style_report.get_count() == 0:
+        style_report = None
+
+    if mypy_exit_error or style_report:
+        print()
+        return (mypy_exit_error, style_report)
+    else:
+        return None
 
 
 if __name__ == '__main__':
