15684a71f88b1a1e23c738336852fc73482c1101
Marcio <dev@marciof.com>
2021-10-06 19:44:12 -0700

    Split feed enclosure sub-directory into its own repository
    See: https://github.com/marciof/feed-enclosure

--- feed-enclosure/scripts/download_jobs_find_db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Prints the path to the download jobs recfile database (may not exist).
-# https://www.gnu.org/software/recutils/
-
-set -e -u
-echo "${XDG_CONFIG_HOME:-$HOME/.config}/feed-enclosure/download-jobs.rec"
--- feed-enclosure/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-# stdlib
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath(
-    os.path.dirname(os.path.dirname(__file__))))
