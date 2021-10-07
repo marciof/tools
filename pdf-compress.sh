fdb493dbd2b35f4dc5dc83c422780eaead315a88
Marcio <dev@marciof.com>
2021-10-06 17:59:25 -0700

    Optionally use trash-cli to move PDF files to trash

--- pdf-compress.sh
+++ pdf-compress.sh
@@ -12,8 +12,11 @@
 # Stdout: tab separated lines, one per PDF filename: original, compressed
 # Stderr: compression status/progress
 #
-# Runtime dependencies:
+# Runtime dependencies (required):
 #   apt install ghostscript # Version: 9.52~dfsg-1ubuntu2 # compress PDF
+#
+# Runtime dependencies (optional):
+#   apt install trash-cli # Version: 0.17.1.14-5build1 # move PDF to trash
 #
 # Test dependencies:
 #   apt install shellcheck # Version: 0.7.1-1build1
@@ -24,29 +27,43 @@ if [ $# -eq 0 ]; then
     set -- *.pdf
 fi
 
+has_trash_put=N
+
+if command -v cat >/dev/null; then
+    has_trash_put=Y
+fi
+
 for pdf_file; do
     if ! [ -r "$pdf_file" ]; then
-        echo "File not available or not readable: $pdf_file" >&2
+        echo "PDF not available or not readable: $pdf_file" >&2
         continue
     fi
 
     compressed_pdf_file="$pdf_file.compressed"
 
-    gs -dNOPAUSE -dBATCH \
-        -sDEVICE=pdfwrite \
-        -dCompatibilityLevel=1.4 \
-        -dPDFSETTINGS=/screen \
-        -sOutputFile="$compressed_pdf_file" \
-        "$pdf_file" >&2
+    (
+        set -x
+        gs -dNOPAUSE -dBATCH \
+            -sDEVICE=pdfwrite \
+            -dCompatibilityLevel=1.4 \
+            -dPDFSETTINGS=/screen \
+            -sOutputFile="$compressed_pdf_file" \
+            -- \
+            "$pdf_file" >&2
+    )
 
-    original_size="$(stat -c %s "$pdf_file")"
-    compressed_size="$(stat -c %s "$compressed_pdf_file")"
+    original_size="$(stat -c %s -- "$pdf_file")"
+    compressed_size="$(stat -c %s -- "$compressed_pdf_file")"
 
-    # TODO use https://github.com/andreafrancia/trash-cli to safely remove original?
-    if [ "$compressed_size" -lt "$original_size" ]; then
-        printf '%s\t%s\n' "$pdf_file" "$compressed_pdf_file"
+    if [ "$compressed_size" -ge "$original_size" ]; then
+        echo "Compressed PDF is larger, removing: $compressed_pdf_file" >&2
+        (set -x; rm -- "$compressed_pdf_file")
+    elif [ "$has_trash_put" = "Y" ]; then
+        echo "Moving original PDF to trash: $pdf_file" >&2
+        (set -x; trash-put -- "$pdf_file")
+        echo "Renaming compressed PDF: $compressed_pdf_file" >&2
+        (set -x; mv -n -- "$compressed_pdf_file" "$pdf_file")
     else
-        echo "Compressed file is larger, removing: $compressed_pdf_file" >&2
-        rm -- "$compressed_pdf_file"
+        printf '%s\t%s\n' "$pdf_file" "$compressed_pdf_file"
     fi
 done
