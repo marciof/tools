f2d548dfc8cf34449c7b89ba50770f7b6e6a2f10
Márcio <dev@marciof.com>
2026-05-22 10:43:23 -0700

    Add draft of alternative solution for char_set_same.py

--- puzzles/char_set_same.py
+++ puzzles/char_set_same.py
@@ -8,42 +8,77 @@ Check if two strings have an identical character set.
 import unittest
 
 
-def has_same_char_set(str_1, str_2):
+def has_same_char_set_v1(str_1: str, str_2: str) -> bool:
     """
-    Time: O(k+l), where k=len(str_1), l=len(str_2)
-    Space: O(k+l), worst-case every character is unique in both strings
+    - Time: O(k+l), where k=len(str_1), l=len(str_2)
+    - Space: O(k+l), worst-case every character is unique in both strings
     """
 
     char_set_1 = set()
     char_set_2 = set()
 
-    for char in str_1:
-        char_set_1.add(char)
+    for char_1 in str_1:
+        char_set_1.add(char_1)
 
-    for char in str_2:
-        if char not in char_set_1:
+    for char_2 in str_2:
+        if char_2 not in char_set_1:
             return False
 
-        char_set_2.add(char)
+        char_set_2.add(char_2)
 
     return len(char_set_1) == len(char_set_2)
 
 
-class Test (unittest.TestCase):
+def has_same_char_set_v2(str_1, str_2) -> bool:
+    """
+    - Time:
+    - Space:
+    """
+
+    char_set_1 = set(str_1)
+    char_set_2 = set()
+
+    for char_2 in str_2:
+        if char_2 in char_set_2:
+            continue
+        elif char_2 not in char_set_1:
+            return False
+        else:
+            char_set_1.remove(char_2)
+            char_set_2.add(char_2)
+
+    return len(char_set_1) == 0
+
+
+class BaseTestCase (unittest.TestCase):
+    has_same_char_set = NotImplementedError
+
+    @classmethod
+    def setUpClass(cls):
+        if cls.has_same_char_set is NotImplementedError:
+            raise unittest.SkipTest(NotImplemented)
+
     def test_empty(self):
-        self.assertTrue(has_same_char_set('', ''))
+        self.assertTrue(self.has_same_char_set('', ''))
 
     def test_superset(self):
-        self.assertFalse(has_same_char_set('aabc', 'bcb'))
+        self.assertFalse(self.has_same_char_set('aabc', 'bcb'))
 
     def test_subset(self):
-        self.assertFalse(has_same_char_set('bcb', 'aabc'))
+        self.assertFalse(self.has_same_char_set('bcb', 'aabc'))
 
     def test_same(self):
-        self.assertTrue(has_same_char_set('abaacab', 'abc'))
+        self.assertTrue(self.has_same_char_set('abaacab', 'abc'))
 
     def test_mix(self):
-        self.assertFalse(has_same_char_set('abaacab', 'ddcbd'))
+        self.assertFalse(self.has_same_char_set('abaacab', 'ddcbd'))
+
+
+class TestCaseV1 (BaseTestCase):
+    has_same_char_set = property(lambda self: has_same_char_set_v1)
+
+class TestCaseV2 (BaseTestCase):
+    has_same_char_set = property(lambda self: has_same_char_set_v2)
 
 
 if __name__ == '__main__':
