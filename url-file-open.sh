78db76ece3eaa52802069a8323c815e99e28b722
Márcio <dev@marciof.com>
2026-06-12 14:25:51 -0700

    Refactor to use the system log as well as the console for verbose/debug output

--- url-file-open.sh
+++ url-file-open.sh
@@ -8,8 +8,26 @@
 # Dependencies (runtime): xdg-utils
 # Dependencies (test): shellcheck
 
-set -o errexit -o nounset -o xtrace
+set -o errexit -o nounset
 
-grep --extended-regexp --max-count 1 '\s*URL\s*=' -- "$1" \
-    | cut --delimiter = --fields 2- \
-    | xargs xdg-open
+SCRIPT_FILENAME="$(basename "$(realpath -e "$0")")"
+
+# Arguments: [output file] ...
+log_and_cat() {
+    while IFS= read -r line; do
+        logger --tag "$SCRIPT_FILENAME" -- "$line"
+        echo "$line"
+
+        for output; do
+            echo "$line" >"$output"
+        done
+    done
+}
+
+for url_file; do
+    log_and_cat <"$url_file" \
+        | grep --extended-regexp --max-count 1 '\s*URL\s*=' \
+        | cut --delimiter = --fields 2- \
+        | log_and_cat /dev/tty \
+        | xargs xdg-open
+done
