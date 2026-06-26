bd63bb6abc20ae474c17e65402a26a9e4523276a
Márcio <dev@marciof.com>
2026-06-25 17:43:09 -0700

    Rename non-public class members to mark them as such

--- kde-plasma-auto-color-scheme.py
+++ kde-plasma-auto-color-scheme.py
@@ -59,11 +59,28 @@ class DesktopAppearanceApi (QObject):
         super().__init__()
         self._logger = logger
 
-        self._logger.debug('Setting up D-Bus session')
+        self._logger.debug('Setting up D-Bus session...')
         self._dbus_session = QDBusConnection.sessionBus()
 
         self._on_color_scheme_change_callbacks: List[Callable[[ColorScheme], None]] = []
         self._is_on_setting_changed_connected = False
+
+
+    @pyqtSlot(str, str, QDBusVariant)
+    def _filter_on_color_scheme_changes(
+            self, namespace: str, key: str, value: QDBusVariant) -> None:
+
+        if namespace != self.APPEARANCE_NAMESPACE:
+            return
+        if key != self.COLOR_SCHEME_KEY:
+            return
+
+        color_scheme_id: int = value.variant()
+        color_scheme = ColorScheme(color_scheme_id)
+        self._logger.info('Color scheme setting changed: %s', color_scheme.name)
+
+        for callback in self._on_color_scheme_change_callbacks:
+            callback(color_scheme)
 
 
     def get_current_color_scheme(self) -> ColorScheme:
@@ -95,23 +112,6 @@ class DesktopAppearanceApi (QObject):
                 self._filter_on_color_scheme_changes)
 
 
-    @pyqtSlot(str, str, QDBusVariant)
-    def _filter_on_color_scheme_changes(
-            self, namespace: str, key: str, value: QDBusVariant) -> None:
-
-        if namespace != self.APPEARANCE_NAMESPACE:
-            return
-        if key != self.COLOR_SCHEME_KEY:
-            return
-
-        color_scheme_id: int = value.variant()
-        color_scheme = ColorScheme(color_scheme_id)
-        self._logger.info('Color scheme setting changed: %s', color_scheme.name)
-
-        for callback in self._on_color_scheme_change_callbacks:
-            callback(color_scheme)
-
-
 class AutoColorScheme (QApplication):
 
     """
@@ -126,8 +126,8 @@ class AutoColorScheme (QApplication):
     def __init__(self, args: List[str]):
         super().__init__(args)
 
-        self.logger = logging.getLogger(self.__class__.__name__)
-        self.logger.setLevel(logging.DEBUG)
+        self._logger = logging.getLogger(self.__class__.__name__)
+        self._logger.setLevel(logging.DEBUG)
 
         syslog_handler = SysLogHandler(address='/dev/log')
         syslog_handler.setFormatter(logging.Formatter(
@@ -137,51 +137,51 @@ class AutoColorScheme (QApplication):
         stdout_handler.setFormatter(logging.Formatter(
             '%(asctime)s: %(message)s'))
 
-        self.logger.addHandler(stdout_handler)
-        self.logger.addHandler(syslog_handler)
+        self._logger.addHandler(stdout_handler)
+        self._logger.addHandler(syslog_handler)
 
-        self.shared_memory = self.ensure_single_instance()
+        self._shared_memory = self._ensure_single_instance()
 
-        (self.read_socket, self.write_socket) = socket.socketpair()
-        self.write_socket.setblocking(False)
-        self.read_socket.setblocking(False)
+        (self._read_socket, self._write_socket) = socket.socketpair()
+        self._write_socket.setblocking(False)
+        self._read_socket.setblocking(False)
 
         # No-op signal handler, quit app in Qt event loop instead.
-        signal.set_wakeup_fd(self.write_socket.fileno())
+        signal.set_wakeup_fd(self._write_socket.fileno())
         signal.signal(signal.SIGINT, lambda sig, frame: None)
 
-        self.socket_notifier = QSocketNotifier(
-            self.read_socket.fileno(), QSocketNotifier.Type.Read, self)
-        self.socket_notifier.activated.connect(self.on_sigint)
-        self.logger.debug('Socket fds for SIGINT handler: read=%s write=%s',
-            self.read_socket.fileno(), self.write_socket.fileno())
+        self._socket_notifier = QSocketNotifier(
+            self._read_socket.fileno(), QSocketNotifier.Type.Read, self)
+        self._socket_notifier.activated.connect(self._on_sigint)
+        self._logger.debug('Socket fds for SIGINT handler: read=%s write=%s',
+                           self._read_socket.fileno(), self._write_socket.fileno())
 
-        self.desktop_appearance_api = DesktopAppearanceApi(self.logger)
+        self._desktop_appearance_api = DesktopAppearanceApi(self._logger)
 
-        self.menu = QMenu()
-        exit_action = QAction('Quit', self.menu)
+        self._menu = QMenu()
+        exit_action = QAction('Quit', self._menu)
         exit_action.triggered.connect(self.quit)
-        self.menu.addAction(exit_action)
+        self._menu.addAction(exit_action)
 
-        self.tray = QSystemTrayIcon()
-        self.tray.setIcon(self.get_current_day_night_cycle_icon())
-        self.tray.setToolTip(APP_NAME)
-        self.tray.setContextMenu(self.menu)
-        self.tray.show()
+        self._tray = QSystemTrayIcon()
+        self._tray.setIcon(self.get_current_day_night_cycle_icon())
+        self._tray.setToolTip(APP_NAME)
+        self._tray.setContextMenu(self._menu)
+        self._tray.show()
 
-        self.desktop_appearance_api.on_color_scheme_change(
+        self._desktop_appearance_api.on_color_scheme_change(
             self.apply_custom_color_scheme)
 
-        self.logger.info('Running...')
+        self._logger.info('Running...')
 
 
-    def ensure_single_instance(self) -> QSharedMemory | SystemExit:
+    def _ensure_single_instance(self) -> QSharedMemory | SystemExit:
         shared_memory = QSharedMemory(
             'com.marciof.tools.kde.plasma.autoColorScheme')
-        self.logger.debug('Shared memory: %s', shared_memory.key())
+        self._logger.debug('Shared memory: %s', shared_memory.key())
 
         if shared_memory.attach() or not shared_memory.create(1):
-            self.logger.info('Another application instance is already running')
+            self._logger.info('Another application instance is already running')
 
             msg = QMessageBox()
             msg.setIcon(QMessageBox.Icon.Warning)
@@ -194,15 +194,15 @@ class AutoColorScheme (QApplication):
         return shared_memory
 
 
-    def quit(self) -> NoReturn:
-        self.logger.info('Quitting...')
-        super().quit()
-
-
-    def on_sigint(self) -> None:
-        self.logger.debug('Received SIGINT (Ctrl+C)')
-        self.socket_notifier.setEnabled(False)
+    def _on_sigint(self) -> None:
+        self._logger.debug('Received SIGINT (Ctrl+C)')
+        self._socket_notifier.setEnabled(False)
         self.quit()
+
+
+    def quit(self) -> NoReturn:
+        self._logger.info('Quitting...')
+        super().quit()
 
 
     def get_day_night_cycle_icon(self, color_scheme: ColorScheme) -> QIcon:
@@ -211,12 +211,12 @@ class AutoColorScheme (QApplication):
 
     def get_current_day_night_cycle_icon(self) -> QIcon:
         return self.get_day_night_cycle_icon(
-            self.desktop_appearance_api.get_current_color_scheme())
+            self._desktop_appearance_api.get_current_color_scheme())
 
 
     def apply_custom_color_scheme(self, color_scheme: ColorScheme) -> None:
-        self.logger.info('Apply color scheme: %s', color_scheme.name)
-        self.tray.setIcon(self.get_day_night_cycle_icon(color_scheme))
+        self._logger.info('Apply color scheme: %s', color_scheme.name)
+        self._tray.setIcon(self.get_day_night_cycle_icon(color_scheme))
 
 
 if __name__ == '__main__':
